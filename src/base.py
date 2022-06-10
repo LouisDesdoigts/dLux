39044eb18ccd9644c9a9658d7a0af9bf98d5eb8d
Louis Desdoigts <ldes9555@uni.sydney.edu.au>
2022-06-10 16:57:57 +1000

    Overhaul of wavefronts class. Now called PhysicalWavefront and has greatly expanded functionality

--- src/base.py
+++ src/base.py
@@ -4,13 +4,12 @@ from jax import vmap
 import equinox as eqx
 from copy import deepcopy
 
-__all__ = ['Wavefront', 'OpticalSystem']
-        
-class Wavefront(eqx.Module):
+class PhysicalWavefront(eqx.Module):
     """
     Class to store the optical information and paramaters
     """
-    wavefront:  np.ndarray
+    amplitude:  np.ndarray
+    phase:      np.ndarray
     wavel:      float
     offset:     np.ndarray
     pixelscale: float
@@ -21,27 +20,158 @@ class Wavefront(eqx.Module):
         self.offset = np.array(offset).astype(float)
         self.planetype = "Pupil"
         self.pixelscale = None
-        self.wavefront = None
-
-    def wf2psf(self):
-        """
-        """
-        return np.abs(self.wavefront)**2
+        self.amplitude = None
+        self.phase = None
+        
+        
+        
+        
+    """Real and Imag"""
+    def get_real(self):
+        return self.ampltidue * np.cos(self.phase)
     
-    def get_XXYY(self, shift=0.):
-        """
-        """
-        npix = self.wavefront.shape[0]
-        xs = np.arange(-npix//2, npix//2) + shift
-        # YY, XX = np.meshgrid(xs, xs)
+    def get_imag(self):
+        return self.ampltidue * np.sin(self.phase)
+    
+    
+    
+    
+    """Updating the Phasor"""
+    def multiply_ampl(self, array_like):
+        new_ampl = self.amplitude * array_like
+        return eqx.tree_at(lambda wf: wf.amplitude, self, new_ampl)
+        
+    def add_phase(self, array_like):
+        new_phase = self.phase + array_like
+        return eqx.tree_at(lambda wf: wf.phase, self, new_phase)
+        
+    def update_phasor(self, amplitude, phase):
+        self = eqx.tree_at(lambda wf: wf.amplitude, self, amplitude)
+        self = eqx.tree_at(lambda wf: wf.phase,     self, phase)
+        return self
+    
+    
+    
+    """Optics stuff"""
+    def wf2psf(self):
+        return self.amplitude**2
+        
+    def add_opd(self, opd):
+        phase = 2*np.pi*opd/self.wavel
+        return self.add_phase(phase)
+        
+    def normalise(self):
+        total_intensity = np.sqrt(np.sum(self.amplitude**2))
+        return self.multiply_ampl(1/total_intensity)
+    
+    
+    
+    
+        
+    """Coordinates"""    
+    def get_xs_vec(self, npix):
+        """ Returns a paraxial 1d vector """
+        return np.arange(npix) - (npix-1)/2 
+    
+    def get_XXYY(self):
+        """ """
+        xs = self.get_xs_vec(self.amplitude.shape[0])
         XX, YY = np.meshgrid(xs, xs)
         return np.array([XX, YY])
     
-    def get_xycoords(self, shift=0.5):
-        """
-        returns x_coords, y,_coords
-        """
-        return self.pixelscale * self.get_XXYY(shift=shift)
+    def get_xycoords(self):
+        """Returns x_coords, y,_coords"""
+        return self.pixelscale * self.get_XXYY()
+    
+    
+    
+    
+    """Interpolation"""
+    def interp(self, coords, reim=False):
+        """ """
+        if not reim:
+            ampl  = map_coordinates(self.amplitude, coords, order=1)
+            phase = map_coordinates(self.phase,     coords, order=1)
+        else:
+            real = map_coordinates(self.get_real, coords, order=1)
+            imag = map_coordinates(self.get_imag, coords, order=1)
+            ampl = np.hypot(real, imag)
+            phase = np.arctan2(imag, real)
+        return ampl, phase
+        
+    def paraxial_interp(self, pixelscale_out, npix_out, reim=False):
+        """ Paraxially interpolates """
+        # Get coords arrays
+        npix_in = self.amplitude.shape[0]
+        ratio = pixelscale_out/self.pixelscale
+        
+        c_in = (npix_in-1)/2
+        c_out = (npix_out-1)/2
+        xs = ratio*(-c_out, c_out, npix_out) + c_in
+        YY, XX = np.meshgrid(xs, xs)
+        coords = np.array([XX, YY])
+        ampl, phase = self.interp(ampl, phase, reim=reim)
+        
+        # Update Phasor
+        self = self.update_phasor(ampl, phase)
+        
+        # Conserve energy
+        self = self.multiply_ampl(ratio)
+        
+        # Update pixelscale
+        return eqx.tree_at(lambda wf: wf.pixelscale, self, pixelscale_out)
+
+    
+    
+    """ Pad and Crop """
+    def pad_to(self, npix_out):
+        """ Pads an array paraxially to a given size
+        Works for even -> even or odd -> odd """
+        npix_in = self.amplitude.shape[0]
+        
+        if npix_in%2 != npix_out%2:
+            raise ValueError("Only supports even -> even or 0dd -> odd padding")
+        
+        c, s, rem = npix_out//2, npix_in//2, npix_in%2
+        padded = np.zeros([npix_out, npix_out])
+        
+        ampl  = padded.at[c-s:c+s+rem, c-s:c+s+rem].set(self.amplitude)
+        phase = padded.at[c-s:c+s+rem, c-s:c+s+rem].set(self.phase)
+        return self.update_phasor(ampl, phase)
+
+    def crop_to(self, npix_out):
+        """ Crops an array paraxially to a given size
+        Works for even -> even or odd -> odd """
+        npix_in = self.amplitude.shape[0]
+        
+        if npix_in%2 != npix_out%2:
+            raise ValueError("Only supports even -> even or 0dd -> odd cropping")
+        
+        c, s = npix_in//2, npix_out//2
+        ampl  = self.amplitude[c-s:c+s, c-s:c+s]
+        phase = self.phase[c-s:c+s, c-s:c+s]
+        return self.update_phasor(ampl, phase)
+        
+        
+        
+        
+    """Inversions"""
+    def invertXY(self):
+        ampl = self.amplitude[::-1, ::-1]
+        phase = self.phase[::-1, ::-1]
+        return self.update_phasor(ampl, phase)
+        
+    def invertX(self):
+        ampl = self.amplitude[:, ::-1]
+        phase = self.phase[:, ::-1]
+        return self.update_phasor(ampl, phase)
+
+    def invertY(self):
+        ampl = self.amplitude[::-1]
+        phase = self.phase[::-1]
+        return self.update_phasor(ampl, phase)
+    
+
     
 class OpticalSystem(eqx.Module):
     """ Optical System class, Equinox Modle
@@ -92,7 +222,6 @@ class OpticalSystem(eqx.Module):
         self.wavels = np.array(wavels).astype(float)
         
         # Set to default values
-        
         self.positions = np.zeros([1, 2]) if positions is None else np.array(positions)
         self.fluxes = np.ones(len(self.positions)) if fluxes is None else np.array(fluxes)
         self.weights = np.ones(1) if weights is None else np.array(weights)
@@ -102,7 +231,7 @@ class OpticalSystem(eqx.Module):
         if self.fluxes.shape == ():
             self.fluxes = np.array([self.fluxes])
         
-        # Determined from inputs
+        # Determined from inputs - treated as static
         self.Nstars =  len(self.positions)
         self.Nims =    len(self.dithers)
         self.Nwavels = 0 if wavels is None else len(self.wavels)
@@ -128,7 +257,8 @@ class OpticalSystem(eqx.Module):
         """
         I believe this is diffable but there is no reason to force it to be
         """
-        params_dict = {"Wavefront": Wavefront(wavel, offset)}
+        # params_dict = {"Wavefront": Wavefront(wavel, offset)}
+        params_dict = {"Wavefront": PhysicalWavefront(wavel, offset)}
         intermeds = []
         layers_applied = []
         for i in range(len(self.layers)):
@@ -181,10 +311,37 @@ class OpticalSystem(eqx.Module):
         """
         
         """
-        params_dict = {"Wavefront": Wavefront(wavel, offset)}
+        # params_dict = {"Wavefront": Wavefront(wavel, offset)}
+        params_dict = {"Wavefront": PhysicalWavefront(wavel, offset)}
         for i in range(len(self.layers)):
             params_dict = self.layers[i](params_dict)
         return params_dict["Wavefront"].wf2psf()
+    
+    def propagate_single(self, wavels, offset=np.zeros(2), weights=1.):
+        """
+        Only propagates a single star, allowing wavelength input
+        sums output to single array
+        
+        Wavels must be an array and the same shape as weights if provided
+        """
+        
+        # Mapping over wavelengths
+        prop_wf_map = vmap(self.propagate_mono, in_axes=(0, None))
+        
+        # Apply spectral weighting
+        psfs = weights * prop_wf_map(wavels, offset)/len(wavels)
+        
+        # Sum into single psf
+        psf = psfs.sum(0)
+        return psf
+    
+    def apply_detector_layers(self, image):
+        """
+        
+        """
+        for i in range(len(self.detector_layers)):
+            image = self.detector_layers[i](image)
+        return image
     
     def reshape_psfs(self, psfs):
         """
@@ -242,28 +399,3 @@ class OpticalSystem(eqx.Module):
         return psfs
         
         
-    def propagate_single(self, wavels, offset=np.zeros(2), weights=1.):
-        """
-        Only propagates a single star, allowing wavelength input
-        sums output to single array
-        
-        Wavels must be an array and the same shape as weights if provided
-        """
-        
-        # Mapping over wavelengths
-        prop_wf_map = vmap(self.propagate_mono, in_axes=(0, None))
-        
-        # Apply spectral weighting
-        psfs = weights * prop_wf_map(wavels, offset)/len(wavels)
-        
-        # Sum into single psf
-        psf = psfs.sum(0)
-        return psf
-    
-    def apply_detector_layers(self, image):
-        """
-        
-        """
-        for i in range(len(self.detector_layers)):
-            image = self.detector_layers[i](image)
-        return image
