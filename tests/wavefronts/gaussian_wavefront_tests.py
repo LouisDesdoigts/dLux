5b639762946d1ab9af059a2283cbc75464f15a1f
Jordan Dennis <jdenn105@gmail.com>
2022-06-23 13:34:29 +1000

    Starting to test the implentation finding contructor bugs.

--- tests/wavefronts/gaussian_wavefront_tests.py
+++ tests/wavefronts/gaussian_wavefront_tests.py
@@ -1,1 +1,11 @@
 import dLux
+import jax.numpy as numpy 
+
+amplitude = numpy.ones((128, 128))
+phase = numpy.zeros((128, 128))
+wavelength = 550e-09
+offset = [0., 0.]
+position = 0.
+beam_radius = 1
+phase_radius = 1 # This quantity is unused and can be removed
+
