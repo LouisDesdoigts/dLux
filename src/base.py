070337b2f4eca628b630697c5d454c66c74f9fe1
Jordan Dennis <jdenn105@gmail.com>
2022-06-29 13:13:56 +1000

    Changing numpy back to np in base.py

--- src/base.py
+++ src/base.py
@@ -1,5 +1,5 @@
 import jax
-import jax.numpy as numpy
+import jax.np as numpy
 from jax import vmap
 import equinox
 from copy import deepcopy
@@ -28,19 +28,19 @@ class OpticalSystem(equinox.Module):
      - Currently doesnt allow temporal variation in flux becuase im lazy
     
     ToDo: Add getter methods for acessing weights and fluxes attributes that
-    use numpy.squeeze to remove empy axes
+    use np.squeeze to remove empy axes
     
     """
-    # Helpers, Determined from inumpyuts, not real params
+    # Helpers, Determined from inputs, not real params
     Nstars:  int
     Nwavels: int
     Nims:    int
     
-    wavels:          numpy.ndarray
-    positions:       numpy.ndarray
-    fluxes:          numpy.ndarray
-    weights:         numpy.ndarray
-    dithers:         numpy.ndarray
+    wavels:          np.ndarray
+    positions:       np.ndarray
+    fluxes:          np.ndarray
+    weights:         np.ndarray
+    dithers:         np.ndarray
     layers:          list
     detector_layers: list
     
@@ -49,43 +49,43 @@ class OpticalSystem(equinox.Module):
     def __init__(self, layers, wavels=None, positions=None, fluxes=None, 
                        weights=None, dithers=None, detector_layers=None):
         
-        # Required Inumpyuts
+        # Required Inputs
         self.layers = layers
-        self.wavels = numpy.array(wavels).astype(float)
+        self.wavels = np.array(wavels).astype(float)
         
         # Set to default values
-        self.positions = numpy.zeros([1, 2]) if positions is None else np.array(positions)
-        self.fluxes = numpy.ones(len(self.positions)) if fluxes is None else np.array(fluxes)
-        self.weights = numpy.ones(len(self.wavels)) if weights is None else np.array(weights)
-        self.dithers = numpy.zeros([1, 2]) if dithers is None else dithers
+        self.positions = np.zeros([1, 2]) if positions is None else np.array(positions)
+        self.fluxes = np.ones(len(self.positions)) if fluxes is None else np.array(fluxes)
+        self.weights = np.ones(len(self.wavels)) if weights is None else np.array(weights)
+        self.dithers = np.zeros([1, 2]) if dithers is None else dithers
         self.detector_layers = [] if detector_layers is None else detector_layers
         
         if self.fluxes.shape == ():
-            self.fluxes = numpy.array([self.fluxes])
+            self.fluxes = np.array([self.fluxes])
         
-        # Determined from inumpyuts - treated as static
+        # Determined from inputs - treated as static
         self.Nstars =  len(self.positions)
         self.Nims =    len(self.dithers)
         self.Nwavels = 0 if wavels is None else len(self.wavels)
         
-        # Check Inumpyut shapes
-        assert self.positions.shape[-1] == 2, """Inumpyut positions must be 
+        # Check Input shapes
+        assert self.positions.shape[-1] == 2, """Input positions must be 
         of shape (Nstars, 2)"""
         
-        assert self.fluxes.shape[0] == self.Nstars, """Inumpyut fluxes must be
-        match inumpyut positions."""
+        assert self.fluxes.shape[0] == self.Nstars, """Input fluxes must be
+        match input positions."""
         
         weight_shape = self.weights.shape
         if len(weight_shape) == 1 and weights is not None:
-            assert weight_shape[0] == self.Nwavels, """Inumpyuts weights shape 
+            assert weight_shape[0] == self.Nwavels, """Inputs weights shape 
             must be either (len(wavels)) or  (len(positions), len(wavels)), 
             got shape: {}""".format(self.weights.shape)
         elif len(weight_shape) == 2:
-            assert weight_shape == [self.Nstars, self.Nwavels], """Inumpyuts 
+            assert weight_shape == [self.Nstars, self.Nwavels], """Inputs 
             weights shape must be either (len(wavels)) or  (len(positions), 
             len(wavels))"""
 
-    def debug_prop(self, wavel, offset=numpy.zeros(2)):        
+    def debug_prop(self, wavel, offset=np.zeros(2)):        
         """
         I believe this is diffable but there is no reason to force it to be
         """
@@ -121,7 +121,7 @@ class OpticalSystem(equinox.Module):
         # Then over the positions 
         propagator = vmap(propagate_single, in_axes=(None, 0))
 
-        # Generate inumpyut positions vector
+        # Generate input positions vector
         dithered_positions = self.dither_positions()
         
         # Calculate PSFs
@@ -137,9 +137,9 @@ class OpticalSystem(equinox.Module):
         detector_vmap = vmap(self.apply_detector_layers, in_axes=0)
         images = detector_vmap(psfs)
         
-        return numpy.squeeze(images)
+        return np.squeeze(images)
 
-    def propagate_mono(self, wavel, offset=numpy.zeros(2)):        
+    def propagate_mono(self, wavel, offset=np.zeros(2)):        
         """
         
         """
@@ -149,9 +149,9 @@ class OpticalSystem(equinox.Module):
             params_dict = self.layers[i](params_dict)
         return params_dict["Wavefront"].wf2psf()
     
-    def propagate_single(self, wavels, offset=numpy.zeros(2), weights=1.):
+    def propagate_single(self, wavels, offset=np.zeros(2), weights=1.):
         """
-        Only propagates a single star, allowing wavelength inumpyut
+        Only propagates a single star, allowing wavelength input
         sums output to single array
         
         Wavels must be an array and the same shape as weights if provided
@@ -179,12 +179,12 @@ class OpticalSystem(equinox.Module):
         """
         
         """
-        numpyix = psfs.shape[-1]
-        return psfs.reshape([self.Nims, self.Nstars, self.Nwavels, numpyix, npix])
+        npix = psfs.shape[-1]
+        return psfs.reshape([self.Nims, self.Nstars, self.Nwavels, npix, npix])
     
     def dither_positions(self):
         """
-        Dithers the inumpyut positions, returned with shape (Npsfs, 2)
+        Dithers the input positions, returned with shape (Npsfs, 2)
         """
         Npsfs = self.Nstars * self.Nims
         shaped_pos = self.positions.reshape([1, self.Nstars, 2])
@@ -196,7 +196,7 @@ class OpticalSystem(equinox.Module):
     def weight_psfs(self, psfs):
         """
         Normalise Weights, and format weights/fluxes
-        Psfs output shape: (Nims, Nstars, Nwavels, numpyix, npix)
+        Psfs output shape: (Nims, Nstars, Nwavels, npix, npix)
         We want weights shape: (1, 1, Nwavels, 1, 1)
         We want fluxes shape: (1, Nstars, 1, 1, 1)
         """
@@ -208,13 +208,13 @@ class OpticalSystem(equinox.Module):
         # Format and normalise weights
         if len(self.weights.shape) == 3:
             weights_in = self.weights.reshape([Nims, Nstars, Nwavels, 1, 1])
-            weights_in /= numpy.expand_dims(weights_in.sum(2), axis=2) 
+            weights_in /= np.expand_dims(weights_in.sum(2), axis=2) 
         elif len(self.weights.shape) == 2:
             weights_in = self.weights.reshape([1, Nstars, Nwavels, 1, 1])
-            weights_in /= numpy.expand_dims(weights_in.sum(2), axis=2) 
+            weights_in /= np.expand_dims(weights_in.sum(2), axis=2) 
         elif self.weights.shape[0] == self.Nwavels:
             weights_in = self.weights.reshape([1, 1, Nwavels, 1, 1])
-            weights_in /= numpy.expand_dims(weights_in.sum(2), axis=2) 
+            weights_in /= np.expand_dims(weights_in.sum(2), axis=2) 
         else:
             weights_in = self.weights
         
