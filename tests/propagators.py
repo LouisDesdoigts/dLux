7f59ef5f554ac4988d38000bdad5bf554d0fe8bc
Jordan Dennis <jdenn105@gmail.com>
2022-07-04 09:23:35 +1000

    Writing the complete set of tests. Still need to implement the utilities

--- tests/propagators.py
+++ tests/propagators.py
@@ -929,6 +929,9 @@ class TestGaussianPropagator(object):
     - _outside_to_inside()
     - _inside_to_outside()
     - _inside_to_inside()
+
+    Some methods have not been tested out of mathematical faith. These 
+    methods are:
     - _waist_to_spherical()
     - _spherical_to_waist()
     - _planar_to_planar()
@@ -945,7 +948,7 @@ class TestGaussianPropagator(object):
     utility : GaussianPropagatorUtility = GaussianPropagatorUtility()
 
 
-    def test_propagate(self : Tester):
+    def test_propagate(self : Tester) -> None:
         """
         Tests full branch coverage and then the boundary cases 
         distance == -numpy.inf, distance == numpy.inf and distance 
@@ -1019,7 +1022,7 @@ class TestGaussianPropagator(object):
         assert not numpy.isinf(outside_to_outside).any()
 
 
-    def test_outside_to_outside_not_nan(self):
+    def test_outside_to_outside(self : Tester) -> None:
         """
         Tests the three boundary cases -numpy.inf, and numpy.inf as
         well as a negative and a positive valid input.
@@ -1031,240 +1034,126 @@ class TestGaussianPropagator(object):
 
         rayleigh_distance = wavefront.rayleigh_distance()
 
-        negative_infinity = set_up().outside_to_outside(-numpy.inf)
-        negative = set_up().outside_to_outside(-rayleigh_distance - 0.01)
-        positive = set_up().outside_to_outside(rayleigh_distance + 0.01)
-        positive_infinity = set_up().outside_to_outside(numpy.inf)
+        negative_infinity = self\
+            .get_utility()\
+            .construct(-numpy.inf)\
+            .outside_to_outside(wavefront\
+                .set_position(rayleigh_distance + 1.))
+
+        negative = self\
+            .get_utility()\
+            .construct(- .2 * rayleigh_distance - 1.)\
+            .outside_to_outside(wavefront\
+                .set_position(rayleigh_distance + .5))
+
+        positive = self\
+            .get_utility()\
+            .construct(2. * rayleigh_distance + 1.)\
+            .outside_to_outside(wavefront\
+                .set_position(- rayleigh_distance - .5))
+
+        positive_infinity = self\
+            .get_utility()\
+            .construct(numpy.inf)\
+            .outside_to_outside(wavefront\
+                .set_position(- rayleigh_ditance - 1.))
         
         assert not numpy.isnan(negative_infinity).any()
         assert not numpy.isnan(negative).any()
         assert not numpy.isnan(positive).any()
         assert not numpy.isnan(positive_infinity).any()     
 
-
-    def test_outside_to_outside_not_inf(self):
-        """
-        Tests the three boundary cases -numpy.inf, and numpy.inf as
-        well as a negative and a positive valid input.
-        """
-        rayleigh_distance = set_up().rayleigh_distance
-        negative_infinity = set_up().outside_to_outside(-numpy.inf)
-        negative = set_up().outside_to_outside(-rayleigh_distance - 0.01)
-        positive = set_up().outside_to_outside(rayleigh_distance + 0.01)
-        positive_infinity = set_up().outside_to_outside(numpy.inf)
-        
         assert not numpy.isinf(negative_infinity).any()
         assert not numpy.isinf(negative).any()
         assert not numpy.isinf(positive).any()
-        assert not numpy.isinf(positive_infinity).any()     
+        assert not numpy.isinf(positive_infinity).any()    
+
+
+    def test_outside_to_inside(self : Tester) -> None:
+        """
+        Tests negative and a positive valid input.
+        """
+        # TODO: Work out if I need to be moving the wavefront
+        # back by the rayleigh_distance in the outside tests
+        wavefront = self\
+            .get_utility()\
+            .get_utility()\
+            .construct()
+
+        rayleigh_distance = wavefront.rayleigh_distance()
+
+        negative = self\
+            .get_utility()\
+            .construct(- rayleigh_distance - 0.01)\
+            .outside_to_outside(wavefront\
+                .set_position(rayleigh_distance + 0.01))
+
+        positive = self\
+            .get_utility()\
+            .construct(rayleigh_distance + 0.01)\
+            .outside_to_outside(wavefront\
+                .set_position(- rayleigh_distance - 0.01))
+        
+        assert not numpy.isnan(negative).any()
+        assert not numpy.isnan(positive).any()
+
+        assert not numpy.isinf(negative).any()
+        assert not numpy.isinf(positive).any()
     
 
-    def test_outside_to_inside_not_nan(self):
+    def test_inside_to_outside(self : Tester) -> None:
         """
         Tests the three boundary cases -numpy.inf, and numpy.inf as
         well as a negative and a positive valid input.
         """
-        rayleigh_distance = set_up().rayleigh_distance
-        negative_infinity = set_up().outside_to_inside(-numpy.inf)
-        negative = set_up().outside_to_inside(-rayleigh_distance - 0.01)
-        positive = set_up().outside_to_inside(rayleigh_distance + 0.01)
-        positive_infinity = set_up().outside_to_inside(numpy.inf)
-        
-        assert not numpy.isnan(negative_infinity).any()
+        wavefront = self\
+            .get_utility()\
+            .get_utility()\
+            .construct()
+
+        rayleigh_distance = wavefront.rayleigh_distance()
+
+        negative = self\
+            .get_utility()\
+            .construct(- rayleigh_distance - 0.01)\
+            .outside_to_outside(wavefront)
+
+        positive = self\
+            .get_utility()\
+            .construct(rayleigh_distance + 0.01)\
+            .outside_to_outside(wavefront)
+
         assert not numpy.isnan(negative).any()
         assert not numpy.isnan(positive).any()
-        assert not numpy.isnan(positive_infinity).any()     
-
-
-    def test_outside_to_inside_not_inf(self):
-        """
-        Tests the three boundary cases -numpy.inf, and numpy.inf as
-        well as a negative and a positive valid input.
-        """
-        rayleigh_distance = set_up().rayleigh_distance
-        negative_infinity = set_up().outside_to_inside(-numpy.inf)
-        negative = set_up().outside_to_inside(-rayleigh_distance - 0.01)
-        positive = set_up().outside_to_inside(rayleigh_distance + 0.01)
-        positive_infinity = set_up().outside_to_inside(numpy.inf)
         
-        assert not numpy.isinf(negative_infinity).any()
         assert not numpy.isinf(negative).any()
         assert not numpy.isinf(positive).any()
-        assert not numpy.isinf(positive_infinity).any()     
-    
 
-    def test_inside_to_outside_not_nan(self):
+
+    def test_inside_to_inside(self : Tester) -> None:
         """
         Tests the three boundary cases -numpy.inf, and numpy.inf as
         well as a negative and a positive valid input.
         """
-        rayleigh_distance = set_up().rayleigh_distance
-        negative_infinity = set_up().inside_to_outside(-numpy.inf)
-        negative = set_up().inside_to_outside(-rayleigh_distance - 0.01)
-        positive = set_up().inside_to_outside(rayleigh_distance + 0.01)
-        positive_infinity = set_up().inside_to_outside(numpy.inf)
-        
-        assert not numpy.isnan(negative_infinity).any()
+        wavefront = self\
+            .get_utility()\
+            .get_utility()\
+            .construct()
+
+        rayleigh_distance = wavefront.rayleigh_distance()
+
+        negative = self\
+            .get_utility()\
+            .construct(- rayleigh_distance + 0.01)\
+            .outside_to_outside(wavefront)
+
+        positive = self\
+            .get_utility()\
+            .construct(rayleigh_distance - 0.01)\
+            .outside_to_outside(wavefront)
+                
         assert not numpy.isnan(negative).any()
         assert not numpy.isnan(positive).any()
-        assert not numpy.isnan(positive_infinity).any()     
-
-
-    def test_inside_to_outside_not_inf(self):
-        """
-        Tests the three boundary cases -numpy.inf, and numpy.inf as
-        well as a negative and a positive valid input.
-        """
-        rayleigh_distance = set_up().rayleigh_distance
-        negative_infinity = set_up().inside_to_outside(-numpy.inf)
-        negative = set_up().inside_to_outside(-rayleigh_distance - 0.01)
-        positive = set_up().inside_to_outside(rayleigh_distance + 0.01)
-        positive_infinity = set_up().inside_to_outside(numpy.inf)
         
-        assert not numpy.isinf(negative_infinity).any()
         assert not numpy.isinf(negative).any()
         assert not numpy.isinf(positive).any()
-        assert not numpy.isinf(positive_infinity).any()     
-
-
-    def test_inside_to_inside_not_nan(self):
-        """
-        Tests the three boundary cases -numpy.inf, and numpy.inf as
-        well as a negative and a positive valid input.
-        """
-        rayleigh_distance = set_up().rayleigh_distance
-        negative_infinity = set_up().inside_to_inside(-numpy.inf)
-        negative = set_up().inside_to_inside(-rayleigh_distance - 0.01)
-        positive = set_up().inside_to_inside(rayleigh_distance + 0.01)
-        positive_infinity = set_up().inside_to_inside(numpy.inf)
-        
-        assert not numpy.isnan(negative_infinity).any()
-        assert not numpy.isnan(negative).any()
-        assert not numpy.isnan(positive).any()
-        assert not numpy.isnan(positive_infinity).any()     
-
-
-    def test_inside_to_inside_not_inf(self):
-        """
-        Tests the three boundary cases -numpy.inf, and numpy.inf as
-        well as a negative and a positive valid input.
-        """
-        rayleigh_distance = set_up().rayleigh_distance
-        negative_infinity = set_up().inside_to_inside(-numpy.inf)
-        negative = set_up().inside_to_inside(-rayleigh_distance - 0.01)
-        positive = set_up().inside_to_inside(rayleigh_distance + 0.01)
-        positive_infinity = set_up().inside_to_inside(numpy.inf)
-        
-        assert not numpy.isinf(negative_infinity).any()
-        assert not numpy.isinf(negative).any()
-        assert not numpy.isinf(positive).any()
-        assert not numpy.isinf(positive_infinity).any()     
-    
-
-    def test_planar_to_planar_not_nan(self):
-        """
-        So I will just test the edge cases, distance = 0. and 
-        distance = numpy.inf and the typical cases distance = 1.
-        and distance = 10.
-        """
-        # TODO: Work out what the fuck is going on with update_phasor 
-        # Should this be a mutable state operation as I have treated it
-        
-        # TODO: Work out what the fuck is going on with update_phasor
-        zero_case = self.set_up().planar_to_planar(0.)
-        infinte_case = self.set_up().planar_to_planar(numpy.inf)
-        small_case = self.set_up().planar_to_planar(1.)
-        large_case = self.set_up().planar_to_planar(10.)
-
-        assert not numpy.isnan(zero_case).any()
-        assert not numpy.isnan(infinite_case).any()
-        assert not numpy.isnan(small_case).any()
-        assert not numpy.isnan(large_case).any()
-
-
-    def test_planar_to_planar_not_inf(self):
-        """
-        So I will test the same cases as above this time looking 
-        for infinite values. Not sure how to consider the case of the 
-        infinite propagation distance
-        """
-        zero_case = self.set_up().planar_to_planar(0.)
-        infinte_case = self.set_up().planar_to_planar(numpy.inf)
-        small_case = self.set_up().planar_to_planar(1.)
-        large_case = self.set_up().planar_to_planar(10.)
-
-        assert not numpy.isinf(zero_case).any()
-        assert not numpy.isinf(infinite_case).any()
-        assert not numpy.isinf(small_case).any()
-        assert not numpy.isinf(large_case).any()
-
-
-    def test_waist_to_spherical_not_nan(self):
-        """
-        Tests the boundary cases distance = 0. and 
-        distance = self.rayleigh_distance as well as two standard 
-        cases.  
-        """
-        rayleigh_distance = self.set_up().rayleigh_distance
-        zero_case = self.set_up().waist_to_spherical(0.)
-        rayleigh_case = self.set_up().waist_to_spherical(rayleigh_distance)
-        small_case = self.set_up().waist_to_spherical(0.01 * rayleigh_distance)
-        large_case = self.set_up().waist_to_spherical(0.9 * rayleigh_distance)
-
-        assert not numpy.isnan(zero_case).any()
-        assert not numpy.isnan(rayleigh_case).any()
-        assert not numpy.isnan(small_case).any()
-        assert not numpy.isnan(large_case).any()
-
-
-    def test_waist_to_spherical_not_inf(self):
-        """
-        Checks that the boundary values and typical values defined in
-        the test above do not generate infinite values
-        """
-        rayleigh_distance = self.set_up().rayleigh_distance
-        zero_case = self.set_up().waist_to_spherical(0.)
-        rayleigh_case = self.set_up().waist_to_spherical(rayleigh_distance)
-        small_case = self.set_up().waist_to_spherical(0.01 * rayleigh_distance)
-        large_case = self.set_up().waist_to_spherical(0.9 * rayleigh_distance)
-
-        assert not numpy.isinf(zero_case).any()
-        assert not numpy.isinf(rayleigh_case).any()
-        assert not numpy.isinf(small_case).any()
-        assert not numpy.isinf(large_case).any()
-
-
-    def test_spherical_to_waist_not_nan(self):
-        """
-        Tests the boundary cases distance = 0. and 
-        distance = self.rayleigh_distance as well as two standard 
-        cases for nan values 
-        """
-        rayleigh_distance = self.set_up().rayleigh_distance
-        zero_case = self.set_up().spherical_to_waist(0.)
-        rayleigh_case = self.set_up().spherical_to_waist(rayleigh_distance)
-        small_case = self.set_up().spherical_to_waist(0.01 * rayleigh_distance)
-        large_case = self.set_up().spherical_to_waist(0.9 * rayleigh_distance)
-
-        assert not numpy.isnan(zero_case).any()
-        assert not numpy.isnan(rayleigh_case).any()
-        assert not numpy.isnan(small_case).any()
-        assert not numpy.isnan(large_case).any()
-
-
-    def test_spherical_to_waist_not_inf(self):
-        """
-        Tests the boundary cases distance = 0. and 
-        distance = self.rayleigh_distance as well as two standard 
-        cases for numpy.inf values 
-        """
-        rayleigh_distance = self.set_up().rayleigh_distance
-        zero_case = self.set_up().spherical_to_waist(0.)
-        rayleigh_case = self.set_up().spherical_to_waist(rayleigh_distance)
-        small_case = self.set_up().spherical_to_waist(0.01 * rayleigh_distance)
-        large_case = self.set_up().spherical_to_waist(0.9 * rayleigh_distance)
-
-        assert not numpy.isinf(zero_case).any()
-        assert not numpy.isinf(rayleigh_case).any()
-        assert not numpy.isinf(small_case).any()
-        assert not numpy.isinf(large_case).any()
