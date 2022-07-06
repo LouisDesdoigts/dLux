Minor bug fixes for the FFT shifting and the normalisation order in the MFT

src/propagators.py
@@ -239,10 +239,13 @@ class Propagator(eqx.Module): # abc.ABC):
         field : Array   
             The electric field of the wavefronts.
         """
-        return jax.lax.cond(self.is_inverse(),
+        field = jax.lax.cond(self.is_inverse(),
             lambda wavefront : self._inverse_fourier_transform(wavefront),
             lambda wavefront : self._fourier_transform(wavefront),
             wavefront)
+
+        field *= self._normalising_factor(wavefront)
+        return field
 
 
     def _normalising_factor(self : Propagator, wavefront : Wavefront) -> float:
@@ -524,8 +527,7 @@ class VariableSamplingPropagator(Propagator):
         """
         wavefront = parameters["Wavefront"]
 
-        new_wavefront = self._normalising_factor(wavefront) * \
-            self._propagate(wavefront)
+        new_wavefront = self._propagate(wavefront)
 
         new_amplitude = np.abs(new_wavefront)
         new_phase = np.angle(new_wavefront)
@@ -584,7 +586,7 @@ class FixedSamplingPropagator(Propagator):
         field : Array[Complex]
             The complex electric field units following propagation.
         """
-        return np.fft.fft2(np.fft.ifftshift(wavefront.get_complex_form()))
+        return np.fft.fftshift(np.fft.ifft2(wavefront.get_complex_form()))
 
 
     def _inverse_fourier_transform(self : Propagator,
@@ -602,7 +604,7 @@ class FixedSamplingPropagator(Propagator):
         field : Array[Complex]
             The complex electric field units following propagation.
         """
-        return np.fft.fftshift(np.fft.ifft2(wavefront.get_complex_form()))
+        return np.fft.fft2(np.fft.ifftshift(wavefront.get_complex_form()))
 
 
     def _normalising_factor(self : Propagator, 
@@ -622,8 +624,8 @@ class FixedSamplingPropagator(Propagator):
             The normalising factor that is appropriate to the 
             method of propagation.
         """
-        return (1 - self.inverse) / wavefront.number_of_pixels() + \
-            self.inverse * wavefront.number_of_pixels()
+        return self.is_inverse() / wavefront.number_of_pixels() + \
+            (1 - self.is_inverse()) * wavefront.number_of_pixels()
 
 
     def get_pixel_scale_out(self : Propagator, 
@@ -663,8 +665,7 @@ class FixedSamplingPropagator(Propagator):
         """
         wavefront = parameters["Wavefront"]
 
-        new_wavefront = self._normalising_factor(wavefront) * \
-            self._propagate(wavefront)
+        new_wavefront = self._propagate(wavefront)
 
         new_amplitude = np.abs(new_wavefront)
         new_phase = np.angle(new_wavefront)
@@ -714,7 +715,8 @@ class PhysicalMFT(VariableSamplingPropagator):
             False.
         """
         super().__init__(inverse = inverse, tilt = tilt,
-            pixel_scale_out = pixel_scale_out, pixels_out = pixels_out) 
+            pixel_scale_out = pixel_scale_out, 
+            pixels_out = pixels_out) 
         self.focal_length = np.array(focal_length).astype(float)
 
 
@@ -927,8 +929,11 @@ class PhysicalFresnel(VariableSamplingPropagator):
         size_out = self.get_pixel_scale_out() * \
             self.get_pixels_out()
         
-        return size_in * size_out / self.get_focal_length() /\
-            wavefront.get_wavelength() * focal_ratio
+        number_of_fringes = size_in * size_out / \
+            self.get_focal_length() / wavefront.get_wavelength() * \
+            focal_ratio
+
+        return number_of_fringes
                
 
     # TODO: Room for optimisation by pasing the radial parameters
@@ -962,29 +967,9 @@ class PhysicalFresnel(VariableSamplingPropagator):
         """
         wavenumber = 2 * np.pi / wavelength
         radial_coordinates = np.hypot(x_coordinates, y_coordinates)
-        return np.exp(-0.5j * wavenumber * radial_coordinates ** 2 \
+        return np.exp(0.5j * wavenumber * radial_coordinates ** 2 \
             / distance)
         
-
-    def thin_lens(self : Propagator, wavefront : Wavefront) -> Array:
-        """
-        A physical usage of the quadratic_phase_factor, that does
-        not have a perfect interpretation.
-
-        Parameters
-        ----------
-        wavefront : Wavefront 
-            The wavefront that is getting focussed.
-
-        Returns
-        -------
-        electric_field : Array
-            The complex electric field. 
-        """
-        field = self.quadratic_phase(*wavefront.get_pixel_positions(),
-            wavefront.get_wavelength(), self.get_focal_length()) 
-        return field
-
 
     def _propagate(self : Propagator, wavefront : Wavefront) -> Array:
         """
@@ -997,35 +982,43 @@ class PhysicalFresnel(VariableSamplingPropagator):
             The wavefront to propagate.
 
         Returns
+        -------
         electric_field : Array
             The complex electric field in the output plane.
         """
         # See gihub issue #52
         offsets = super().get_pixel_offsets(wavefront) 
-        
+    
         input_positions = wavefront.get_pixel_positions()
         output_positions = self._get_pixel_grid(
             offsets, self.get_pixel_scale_out(),
             self.get_pixels_out())
 
-        number_of_fringes = self.number_of_fringes(wavefront)
         propagation_distance = self.get_focal_length() + self.get_focal_shift()
 
         field = wavefront.get_complex_form()
-        field *= self.thin_lens(wavefront)
+        print("initial: ", np.sum(np.abs(field) ** 2))
+        field *= self.quadratic_phase(*input_positions,
+            wavefront.get_wavelength(), -self.get_focal_length())
+        print("thin lens: ", np.sum(np.abs(field) ** 2))
         field *= self.quadratic_phase(*input_positions, 
             wavefront.get_wavelength(), propagation_distance)
-        
+        print("rho_1: ", np.sum(np.abs(field) ** 2))
+
         amplitude = np.abs(field)
         phase = np.angle(field)
         wavefront = wavefront.update_phasor(amplitude, phase)
 
         # Gives the inverse capability
         field = super()._propagate(wavefront) 
+        print("_propagate:", np.sum(np.abs(field) ** 2))
         field *= wavefront.get_pixel_scale() ** 2
+        print("pixel_scale:", np.sum(np.abs(field) ** 2))
         field *= wavefront.transfer_function(propagation_distance)
+        print("transfer_function: ", np.sum(np.abs(field) ** 2))
         field *= self.quadratic_phase(*output_positions, 
             wavefront.get_wavelength(), propagation_distance)
+        print("rho_2: ", np.sum(np.abs(field) ** 2))
 
         return field 
 

tests/integration.py
@@ -9,6 +9,11 @@ that then need to be manually checked.
 from dLux import *
 from typing import NewType
 from matplotlib import pyplot
+import equinox as eqx
+import jax.numpy as np
+import jax
+
+jax.config.update("jax_enable_x64", True)
 
 Telescope = NewType("Telescope", OpticalSystem)
 
@@ -123,7 +128,7 @@ class Toliman(OpticalSystem):
         detector_pixels : int
             The number of pixels in the ouput layer
         """
-        return self.detector_layers
+        return self.detector_pixels
 
 
     def get_outer_aperture_radius(self : Telescope) -> float:
@@ -191,6 +196,17 @@ class Toliman(OpticalSystem):
         return self.pixel_scale_out
 
 
+    def set_focal_shift(self : Telescope, shift : float) -> Telescope:
+        """
+        Parameters
+        ----------
+        shift : float, meters
+            The focal shift of the detector plane.
+        """
+        return eqx.tree_at(lambda toliman : toliman.focal_shift, 
+            self, shift) 
+
+
     # TODO: This is very shit.
     def set_propagator(self : Telescope, in_focus : bool, fixed : bool) -> None:
         """
@@ -205,38 +221,53 @@ class Toliman(OpticalSystem):
             be used.
         """
         if fixed:
-            layers.append(PhysicalFFT(
-                inverse = False,
+            self.layers.append(PhysicalFFT(
                 focal_length = self.get_focal_length()))
         else:
             if in_focus:
-                layers.append(PhysicalMFT(
+                self.layers.append(PhysicalMFT(
                     pixels_out = self.get_detector_pixels(),
                     focal_length = self.get_focal_length(), 
                     pixel_scale_out = self.get_pixel_scale_out() / \
-                        self.get_pixel_oversample(), 
-                    inverse = False, 
-                    tilt = False))
+                        self.get_pixel_oversample()))
             else:
-                layers.append(PhysicalFresnel(
+                self.layers.append(PhysicalFresnel(
                     pixels_out = self.get_detector_pixels(), 
                     focal_length = self.get_focal_length(), 
                     focal_shift = self.get_focal_shift(),
                     pixel_scale_out = self.get_pixel_scale_out() /\
-                        self.get_pixel_oversample(), 
-                    inverse = False,
-                    tilt = False))
+                        self.get_pixel_oversample()))
 
-mft_toliman = Toliman()
-mft_toliman.set_propagator(in_focus = True, fixed = False)
-fft_toliman = Toliman()
-fft_toliman.set_propagator(in_focus = True, fixed = True)
-fnl_toliman = Toliman()
-fnl_toliman.set_propagator(in_focus = False, fixed = False)
+        return self
 
-mft_image = mft_toliman() ** 0.5
-fft_image = fft_toliman() ** 0.5
-fnl_image = fnl_toliman() ** 0.5
+mft_toliman = Toliman().set_propagator(in_focus = True, fixed = False)
+fft_toliman = Toliman().set_propagator(in_focus = True, fixed = True)
+fnl_toliman = Toliman().set_propagator(in_focus = False, fixed = False)
+
+mft_image, intermediate_wavefronts, layer = fnl_toliman.debug_prop(550e-09)
+print("MFT")
+for i, intermediate_wavefront in enumerate(intermediate_wavefronts):
+    intermediate_wavefront = intermediate_wavefront["Wavefront"]
+    print(f"{layer[i]}: ", np.sum(intermediate_wavefront.get_amplitude() ** 2))
+    
+    pyplot.figure(figsize = (10, 5))
+    pyplot.subplot(1, 2, 1)
+    pyplot.imshow(intermediate_wavefront.get_amplitude())
+    pyplot.colorbar()
+    pyplot.subplot(1, 2, 2) 
+    pyplot.imshow(intermediate_wavefront.get_phase())
+    pyplot.colorbar()
+    pyplot.show()
+
+#fft_image, intermediate_wavefronts, layer = fft_toliman.debug_prop(550e-09)
+#fnl_image, intermediate_wavefronts, layer = fnl_toliman.debug_prop(550e-09)
+
+mft_image = mft_toliman()
+print(np.sum(mft_image))
+fft_image = fft_toliman()
+print(np.sum(fft_image))
+fnl_image = fnl_toliman()
+print(np.sum(fnl_image))
  
 pyplot.figure(figsize = (15, 4))
 pyplot.subplot(1, 3, 1)
@@ -252,3 +283,16 @@ pyplot.title("FNL PSF")
 pyplot.imshow(fnl_image)
 pyplot.colorbar()
 pyplot.show()
+
+#fnl_in_front = Toliman()\
+#    .set_focal_shift(1.32 / 1000)\
+#    .set_propagator(in_focus = False, fixed = False)
+#    
+#fnl_in_front_image = fnl_in_front() ** 0.5
+
+#pyplot.figure(figsize = (5, 5))
+#pyplot.title("Out of Focus")
+#pyplot.imshow(fnl_in_front_image)
+#pyplot.colorbar()
+#pyplot.show()
+