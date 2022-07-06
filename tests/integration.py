75b155c77e16b480ecc81593c00d691573f9faf0
Jordan Dennis <jdenn105@gmail.com>
2022-07-06 10:57:36 +1000

    Significantly upgrading the Toliman class and having it ready to generate the Airy pattern for all of the PhysicalWavefronts

--- tests/integration.py
+++ tests/integration.py
@@ -7,9 +7,10 @@ that then need to be manually checked.
 """
 
 from dLux import *
+from typing import NewType
+from matplotlib import pyplot
 
-# NOTE: So I plan to be using the Toliman. Let's get this running 
-# and then fix the tilt problem in the propagators.
+Telescope = NewType("Telescope", OpticalSystem)
 
 class Toliman(OpticalSystem):
     """
@@ -52,13 +53,12 @@ class Toliman(OpticalSystem):
             extra_layers : list = [],
             wavefront_pixels : int = 256,
             detector_pixels : int = 256,
-            outer_aperture_radius : float = 0.125
+            outer_aperture_radius : float = 0.125,
             inner_aperture_radius : float = 0.02,
             focal_shift : float = 0.,
             focal_length : float = 1.32,
             pixel_oversample : int = 5, 
-            pixel_scale_out : float = 6.5e-06,
-            in_focus : bool = True):
+            pixel_scale_out : float = 6.5e-06):
         """
         Parameters
         ----------
@@ -103,21 +103,135 @@ class Toliman(OpticalSystem):
             NormaliseWavefront()]
     
         layers.extend(extra_layers)
-    
-        if in_focus:
-            layers.append(PhysicalMFT(
-                pixels_out = detector_pixels,
-                focal_length = focal_length, 
-                pixel_scale_out = pixel_scale_out / pixel_oversample, 
-                inverse = False, 
-                tilt = False))
-        else:
-            layers.append(PhysicalFresnel(
-                pixels_out = detector_pixels, 
-                focal_length = focal_length, 
-                focal_shift = focal_shift,
-                pixel_scale_out = pixel_scale_out / pixel_oversample, 
+        super().__init__(layers, wavels=[550e-09])
+
+
+    def get_wavefront_pixels(self : Telescope) -> int:
+        """
+        Returns
+        -------
+        wavefront_pixels : int
+            The number of pixels in the input layer.
+        """
+        return self.wavefront_pixels
+
+
+    def get_detector_pixels(self : Telescope) -> int:
+        """
+        Returns
+        -------
+        detector_pixels : int
+            The number of pixels in the ouput layer
+        """
+        return self.detector_layers
+
+
+    def get_outer_aperture_radius(self : Telescope) -> float:
+        """
+        Returns
+        -------
+        outer_aperture_radius : float
+            The radius of the outer rim of the annular toliman 
+            aperture in meters.
+        """
+        return self.outer_aperture_radius
+
+
+    def get_inner_aperture_radius(self : Telescope) -> float:
+        """
+        Returns
+        -------
+        inner_aperture_radius : float
+            The radius of the inner rim of the annular toliman 
+            aperture.
+        """
+        return self.inner_aperture_radius
+
+
+    def get_focal_shift(self : Telescope) -> float:
+        """
+        Returns 
+        -------
+        focal_shift : float
+            The displacement of the detector layer from the focal
+            plane of the toliman telescope.
+        """
+        return self.focal_shift
+
+ 
+    def get_focal_length(self : Telescope) -> float:
+        """
+        Returns
+        -------
+        focal_length : float
+            The focal length of the toliman telescope in meters.
+        """
+        return self.focal_length
+
+
+    def get_pixel_oversample(self : Telescope) -> float:
+        """
+        Returns
+        -------
+        oversample : int
+            The number of simulated pixels per physical pixel in this 
+            simulation of the toliman optical train.
+        """
+        return self.pixel_oversample
+
+
+    def get_pixel_scale_out(self : Telescope) -> float:
+        """
+        Returns
+        -------
+        pixel_scale_out : float 
+            The side length of a physical pixel in the toliman detector
+            plane in meters.
+        """
+        returns self.pixel_scale_out
+
+
+    # TODO: This is very shit.
+    def set_propagator(self : Telescope, in_focus : bool, fixed : bool) -> None:
+        """
+        Choose your propagator.
+
+        Parameters
+        ----------
+        in_focus : bool
+            True if the MFT is to be used else the fresnel will be used.
+        fixed : bool
+            Can only be set if in focus is True. True if the FFT is to 
+            be used.
+        """
+        if fixed:
+            layers.append(PhysicalFFT(
                 inverse = False,
-                tilt = False))
+                focal_length = self.get_focal_length()))
+        else:
+            if in_focus:
+                layers.append(PhysicalMFT(
+                    pixels_out = self.get_detector_pixels(),
+                    focal_length = self.get_focal_length(), 
+                    pixel_scale_out = self.get_pixel_scale_out() / \
+                        self.get_pixel_oversample(), 
+                    inverse = False, 
+                    tilt = False))
+            else:
+                layers.append(PhysicalFresnel(
+                    pixels_out = self.get_detector_pixels(), 
+                    focal_length = self.get_focal_length(), 
+                    focal_shift = self.get_focal_shift(),
+                    pixel_scale_out = self.get_pixel_scale_out() /\
+                        self.get_pixel_oversample(), 
+                    inverse = False,
+                    tilt = False))
+
     
-        self.layers = layers
+image = Toliman()() ** 0.5
+
+pyplot.figure(figsize = (10, 10))
+pyplot.title("PSF")
+pyplot.imshow(image)
+pyplot.colorbar()
+pyplot.show()
