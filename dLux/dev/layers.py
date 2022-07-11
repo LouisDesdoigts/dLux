cd46665f52679fa4b327c9662064bfb683bdca57
Jordan Dennis <jdenn105@gmail.com>
2022-07-11 11:17:50 +1000

    Starting work on #59 while developing the hexikes in their most general form

--- dLux/dev/layers.py
+++ dLux/dev/layers.py
@@ -157,22 +157,63 @@ class HexagonalAperture(Aperture):
     rmax : float
         The proportion of the pixel vector that is taken up by a 
         circle containing the hexagon.
+    x : int
+        The pixel coordinate in the x direction of the centre of the 
+        hexagonal aperture.
+    y : int
+        The pixel coordinate in the y direction of the centre of the 
+        hexagonal aperture. 
     """
     _rmax : float
+    _x : int
+    _y : int
 
 
-    def __init__(self : Layer, npix : int, rmax : float) -> Layer:
+    def __init__(self : Layer, 
+            number_of_pixels_on_side : int, 
+            proportional_maximum_radius : float,
+            x_position_of_centre : int = 0, 
+            y_position_of_centre : int = 0) -> Layer:
         """
         Parameters
         ----------
-        npix : int
+        number_of_pixels_on_side : int
             The number of pixels along one side of the square array
-        rmax : float 
+        proportional_maximum_radius : float 
             The outer radius of the smallest circle that contains the 
             hexagon.
+        x_position_of_centre : int = 0
+            The x position of the centre of the hexagonal aperture.
+        y_poxition_of_centre : int = 0
+            The y position of the centre of the hexagonal aperture.             
         """
-        super().__init__(npix)
-        self.rmax = rmax
+        super().__init__(number_of_pixels_on_side)
+        self._rmax = proportional_maximum_radius
+        self._x = x_position_of_centre
+        self._y = y_position_of_centre
+
+
+    def _get_centre(self : Layer) -> tuple:
+        """
+        The x and y coordinates of the centre of the hexagonal aperture.
+        
+        Returns
+        -------
+        x, y : tuple
+            The x and y coordinates of the centre in pixels
+        """
+        return self._x, self._y
+
+
+    def _get_rmax(self : Layer) -> float:
+        """
+        Returns
+        -------
+        rmax : float
+            The proportional maximum radius of the smallest circle that
+            can completely enclose the aperture.
+        """
+        return self._rmax
 
 
     def _aperture(self : Layer) -> Array:
@@ -187,8 +228,12 @@ class HexagonalAperture(Aperture):
             1. representing no transmission and transmission 
             respectively.
         """
-        x, y = _get_pixel_positions(number_of_pixels, x_pixel_offset,
-            y_pixel_offset)
+        x_centre, y_centre = self._get_centre()
+        number_of_pixels = self._get_npix()
+        maximum_radius = self._get_rmax()
+
+        x, y = _get_pixel_positions(number_of_pixels, -x_centre,
+            -y_centre)
 
         x *= 2 / number_of_pixels
         y *= 2 / number_of_pixels
@@ -206,6 +251,224 @@ class HexagonalAperture(Aperture):
 
         hexagon = rectangle | left_triangle | right_triangle
         return np.asarray(hexagon).astype(float)
+
+
+class BasisPhase(Layer, ABC):
+    """
+    _Abstract_ class representing a basis fixed over an aperture 
+    that is used to optimise and learn aberations in the aperture. 
+    
+    Attributes
+    ----------
+    _x : float
+        The x coordinate of the centre of the aperture.
+    _y : float
+        The y coordinate of the centre of the aperture.
+    _r : float
+        The radius of the aperture. The radius belongs to the smallest
+        circle that completely contains the aperture. For the math
+        nerds the infimum of the set of circles whose union with the
+        aperture is the aperture. 
+    _npix : int
+        The number of pixels along the edge of the square array 
+        representing each term in the basis.
+    """
+    _x : float
+    _y : float
+    _r : float
+    _npix : int
+
+
+    def __init__(self : Layer, x : float, y : float, r : float,
+            npix : int) -> Layer:
+        """
+        Parameters
+        ----------
+        x : float
+            The x coordinate of the centre of the aperture containing 
+            the basis.
+        y : float
+            The y coordinate of the centre of the aperture containing 
+            the basis.
+        r : float
+            The radius of the aperture containing the basis. For 
+            abitrary shapes the radius is belongs to the smallest 
+            circle that can completely contain the aperture. 
+        npix : int
+            The number of pixels along one side of the basis arrays.
+            That is each term in the basis will be evaluated on a 
+            `npix` by `npix` grid.
+        """
+        self._x = np.asarray(x).astype(float)
+        self._y = np.asarray(y).astype(float)
+        self._r = np.asarray(r).astype(float)
+        self._npix = int(npix)
+
+
+    @abstractmethod
+    def _aperture(self : Layer) -> Matrix:
+        """
+        Generate a binary or soft edged aperture mask in which to 
+        construct the basis polynomials. 
+
+        Returns
+        -------
+        aperture : Matrix
+            The aperture encoded in an `npix` by `npix` array. Note 
+            that with hard edged apertures the gradient can become 
+            unusual.
+        """
+
+
+    @abstractmethod
+    @cached_property 
+    def _basis(self : Layer, n : int) -> Tensor:
+        """
+        Generate the basis. Requires a single run after which,
+        the basis is cached and can be used with no computational 
+        cost.  
+
+        Parameters
+        ----------
+        n : int 
+            The number of terms in the basis to generate. 
+
+        Returns
+        -------
+        basis : Tensor
+            The basis polynomials evaluated on the square arrays
+            containing the apertures until `maximum_radius`.
+            The leading dimension is `n` long and 
+            each stacked array is a basis term. The final shape is:
+            ```py
+            basis.shape == (n, npix, npix)
+            ```
+        """
+        # TODO: This may cause `jit`ting problems in some cases. It
+        # will force me to work on the zernike `jit`
+
+
+    def save(self : Layer, file_name : str, n : int) -> None:
+        """
+        Save the basis to a file.
+
+        Parameters
+        ----------
+        file_name : str
+            The name of the file to save the basis terms to.
+        n : int
+            The number of terms in the basis to generate in the save.
+        """
+        basis = self._basis
+        with open(file_name, "w") as save:
+            save.write(basis)
+        # TODO: Implement either a LoadedBasis abstract class 
+        # or add conditional logic for a load method. 
+
+
+    def __call__(self : Layer, parameters : dict) -> dict:
+        """
+        Apply a phase shift to the wavefront based on the basis 
+        terms that have been generated.
+
+        Overrides
+        ---------
+        __call__ : Layer
+            Provides a concrete implementation of the `__call__` method
+            defined in the abstract base class `Layer`.
+
+        Parameters
+        ----------
+        parameters : Dict
+            A dictionary containing the parameters of the model. 
+            The dictionary must satisfy `parameters.get("Wavefront")
+            != None`. 
+
+        Returns
+        -------
+        parameters : Dict
+            The parameter, parameters, with the "Wavefront"; key
+            value updated. 
+        """
+        wavefront = parameters["Wavefront"]
+        parameters["Wavefront"] = wavefront.add_phase(self._basis)
+        return parameters
+        
+
+class PolygonBasis(PhaseBasis, ABC):
+    """
+    Orthonormalises the zernike basis over a polygon to provide
+    a basis on a polygonal aperture.
+    """
+    @abstractmethod
+    def _get_vertices(self : Layer) -> Matrix:
+        """
+        Returns
+        -------
+        vertices : Matrix
+            A 2 by number of vertices matrix containing the x 
+            x coordinates along the 0th row and the y coordinates
+            along the 1st row. The nth column is the x and y coordinates
+            of the nth vertice. 
+        """
+
+
+    def _noll_index(self : Layer, j : int) -> tuple:
+        """
+        Decode the jth noll index of the zernike polynomials. This 
+        arrises because the zernike polynomials are parametrised by 
+        a pair numbers, e.g. n, m, but we want to impose an order.
+        The noll indices are the standard way to do this see [this]
+        (https://oeis.org/A176988) for more detail. The top of the 
+        mapping between the noll index and the pair of numbers is 
+        shown below:
+
+        n, m Indices
+        ------------
+        (0, 0)
+        (1, -1), (1, 1)
+        (2, -2), (2, 0), (2, 2)
+        (3, -1), (3, -1), (3, 1), (3, 3)
+
+        Noll Indices
+        ------------
+        1
+        3, 2
+        5, 4, 6
+        9, 7, 8, 10
+        15, 13, 11, 12, 14
+
+        Notes
+        -----
+        - The noll indices always start with the largest odd number 
+          in the row on the left. We then "bounce", for want of a 
+          better word from side to side across the triangle 
+          incrementing by +1 or -1 depending on the whether the 
+          largest odd number is the largest number in the row. 
+
+        Parameters
+        ----------
+        j : int
+            The noll index to decode.
+        
+        Returns
+        -------
+        n, m : tuple
+            The n, m parameters of the zernike polynomial.
+        """
+                
+
+    def _zernike(self : Layer, n : int, m : int) -> Tensor: 
+
+
+    @cached_property
+    def _basis(self : Layer):
+
+
+class HexagonalBasis(PolygonBasis)
+
+    
+
 
 
 def hexike_basis(
