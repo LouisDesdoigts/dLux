31bad3bb6bf387da5980b49b99ccb8a1cf034077
Jordan Dennis <jdenn105@gmail.com>
2022-07-14 16:06:12 +1000

    get_radial_positions is not offsetting correctly

--- dLux/dev/segmented_mirror.py
+++ dLux/dev/segmented_mirror.py
@@ -11,6 +11,13 @@ angles += (angles < 0.) * (2 * np.pi + angles)
 
 radial = np.hypot(vertices[0], vertices[1])
 
+# Wrap required
+x = vertices[np.arange(-1, 6), 0].reshape(7, 1, 1)
+y = vertices[np.arange(-1, 6), 1].reshape(7, 1, 1)
+
+print(x)
+print(y)
+
 # NOTE: Simple hexagonal case. 
 #angles = np.linspace(0, 2 * np.pi, 7, endpoint=True).reshape(7, 1, 1)
 #x = np.cos(angles).reshape(7, 1, 1)
@@ -20,7 +27,15 @@ a = (x[1:] - x[:-1])
 b = (y[1:] - y[:-1])
 c = (a * y[:-1] - b * x[:-1])
 
-positions = get_radial_positions(256, 0, 0)
+# All that needs to be done is that the positions be translated to
+# the correct place
+x_offset = np.mean(x)
+y_offset = np.mean(y)
+
+print(vertices)
+print(x_offset)
+# TODO: Test the get_radial_positions for offset again.
+positions = get_radial_positions(256, x_offset, y_offset)
 rho = (positions[0] * 2 / 256)
 theta = (positions[1] + np.pi)[:, ::-1]
 rho = np.tile(rho, (6, 1, 1))
@@ -30,7 +45,8 @@ linear = (-c / (a * np.sin(theta) + b * np.cos(theta)))
 angular = ((angles[:-1] < theta) & (theta < angles[1:]))[::-1, :, :] 
 test = (rho < linear) & angular
 
-pyplot.imshow(test.sum(axis = 0))
+# pyplot.imshow(test.sum(axis = 0))
+pyplot.imshow(rho[0])
 pyplot.colorbar()
 pyplot.show()
 
