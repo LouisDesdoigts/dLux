de7051a915b8eef790dc2373cfb1b7a7ace3676f
Jordan Dennis <jdenn105@gmail.com>
2022-07-14 15:56:42 +1000

    Generating a hexagonal aperture from the vertices

--- dLux/dev/segmented_mirror.py
+++ dLux/dev/segmented_mirror.py
@@ -12,27 +12,25 @@ angles += (angles < 0.) * (2 * np.pi + angles)
 radial = np.hypot(vertices[0], vertices[1])
 
 # NOTE: Simple hexagonal case. 
-angles = np.linspace(0, 2 * np.pi, 7, endpoint=True).reshape(7, 1, 1)
-vertices = np.array([np.cos(angles), np.sin(angles)]).reshape(7, 2, 1)
+#angles = np.linspace(0, 2 * np.pi, 7, endpoint=True).reshape(7, 1, 1)
+#x = np.cos(angles).reshape(7, 1, 1)
+#y = np.sin(angles).reshape(7, 1, 1)
 
-a = (vertices[1:, 0] - vertices[:-1, 0]).reshape(6, 1, 1)
-b = (vertices[1:, 1] - vertices[:-1, 1]).reshape(6, 1, 1)
-c = ((vertices[1:, 0] - vertices[:-1, 0]) * vertices[:-1, 1] -\
-    (vertices[1:, 1] - vertices[:-1, 1]) * vertices[:-1, 0])\
-    .reshape(6, 1, 1)
+a = (x[1:] - x[:-1])
+b = (y[1:] - y[:-1])
+c = (a * y[:-1] - b * x[:-1])
 
 positions = get_radial_positions(256, 0, 0)
-rho = positions[0] * 2 / 256
+rho = (positions[0] * 2 / 256)
 theta = (positions[1] + np.pi)[:, ::-1]
 rho = np.tile(rho, (6, 1, 1))
 theta = np.tile(theta, (6, 1, 1))
 
-linear = (c / (a * np.sin(theta) + b * np.cos(theta)))
-angular = ((angles[:-1] < theta) & (theta < angles[1:])) 
-test = (rho < linear) & (angular)
+linear = (-c / (a * np.sin(theta) + b * np.cos(theta)))
+angular = ((angles[:-1] < theta) & (theta < angles[1:]))[::-1, :, :] 
+test = (rho < linear) & angular
 
-print(angular.shape)
- 
-pyplot.imshow(theta[0])
+pyplot.imshow(test.sum(axis = 0))
 pyplot.colorbar()
 pyplot.show()
+
