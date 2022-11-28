d529f920b2348ed8cb62b512f053f4e9a73fa6de
Adam Taras <ataras2@gmail.com>
2022-11-28 16:37:12 +1100

    testing single basis

---
+++ dLux/dev/tests/t_basis.py
@@ -0,0 +1,44 @@
+__author__ = "ataras2"
+
+import dLux as dl
+
+import dLux.basis as basis
+
+import jax.numpy as np
+
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    circ1_pos = [ 0.2,0.3]
+    circ2_pos = [-0.2,0]
+
+    circ1_r = 0.1
+    circ2_r = 0.1
+
+    n_pix = 128
+    coordinates = dl.utils.get_pixel_coordinates(n_pix, 1. / n_pix)
+
+
+    # test with normal
+    circ1 = dl.CircularAperture(x_offset=circ1_pos[0], y_offset=circ1_pos[1], radius=circ1_r, occulting=False, softening=True)
+    circ2 = dl.CircularAperture(x_offset=circ2_pos[0], y_offset=circ2_pos[1], radius=circ2_r, occulting=False, softening=True)
+
+    compund_circs = dl.CompoundAperture({'c1' : circ1, 'c2' : circ2}, use_prod=False)
+    plt.figure()
+    plt.subplot(1,2,1)
+    plt.imshow(circ1._aperture(coordinates))
+    plt.subplot(1,2,2)
+    plt.imshow(compund_circs._aperture(coordinates))
+    plt.show()
+
+
+    circ1_basis = basis.Basis(nterms=5, aperture=circ1)
+
+    plt.figure()
+    # b = circ1_basis.basis(coordinates=coordinates)
+    b = circ1_basis._zernikes(coordinates=coordinates)
+    for i in range(b.shape[0]):
+        plt.subplot(1,b.shape[0],i+1)
+        plt.imshow(np.squeeze(b[i,:,:]))
+        
+    plt.show()
