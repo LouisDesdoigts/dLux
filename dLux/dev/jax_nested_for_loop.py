99d08fca7c6f9382a9a8fc0106e200bbb83c99b6
Jordan Dennis <jdenn105@gmail.com>
2022-11-29 11:15:08 +1100

    Really getting stuck in th thick of the problem

--- dLux/dev/jax_nested_for_loop.py
+++ dLux/dev/jax_nested_for_loop.py
@@ -1,7 +1,7 @@
 import jax.numpy as np
 import jax 
 
-@jax.jit
+
 def jit_nested_for():
     """
     The problem that this is trying to solve is.
@@ -66,8 +66,67 @@ def jit_nested_for():
     tot_len: int = int(n * (n + 1) / 2)
     shape: tuple = (2, tot_len) 
 
-    i: int = np.arange(n, dtype=int)
+    i: int = np.arange(1, n + 1, dtype=int)
     j: int = np.zeros(shape, dtype=int)
+    
+    j: int = j.at[0].set(np.repeat(i, i, total_repeat_length=tot_len))
+
+    # Now comes the problem of the inner for loop. I cannot use `for` 
+    # because the loop variable is not static. However, I need to 
+    # apply `np.arange` to multiple different values. Essentially,
+    # I am looking for `np.arange(np.arange())`. This is a folly 
+    # bacause the size of the array is unkown.
+
+    # OK I'm starting to formulate a plan. So if I have an `(n, tot_len)` 
+    # array of ones, I can zero out below the diagonal and then sum 
+    # along the vertical axis to get the values that I need. Haha, yes!
+    # For example,
+    #
+    #     1 1 1 1 1 1 => 63 
+    #     0 0 1 0 1 1 => 11
+    #     0 0 0 0 0 1 =>  1
+    # sum 1 1 2 1 2 3
+    #
+    # Can this be done with `lax.scan`? Or perhaps I can be very clever 
+    # and create the encoding above from a binary image of `n`? 
+    #
+    # n: 0 => b0
+    # n: 1 => b1
+    # n: 2 => b10
+    # n: 3 => b11
+    # n: 4 => b100
+    # 
+    # So b11...1 can be created how? Well it is the sum over `2 >> i`.
+    # I cannot remeber if the `operator` library employs the vectorisable
+    # versions of bitwise operations. It does, nice!
+    #
+    # np.sum(jax.vmap(operator.lshift)(i))
+    # np.sum(jax.vmap(lambda x: 2 << x)(i))
+    # np.sum(jax.vmap(lambda x: 2 ** x)(i))
+    #
+    # I'll use the bottom one for now but the other two present optimisation 
+    # opportunities. 
+
+    # While I hvae made some progress above I do not think that it is 
+    # going to lead to a readable solution soon. Let me write done the 
+    # naive approach. 
+    #
+
+    len_of_arange: callable = lambda x: x * (x + 1) / 2
+    vmap_len_of_arange: callable = jax.vmap(len_of_arange)
+
+    # Let me work through some examples,
+    #
+    #              1 | 1 2 | 1 2 3 | 1 2 3 4 |
+    # start_index: 0 | 1   | 3     | 6   
+    # length:      1 | 2   | 3     | 4
+    #
+    # God this is a slog. Just think: if I can get it, the reward will 
+    # be worth it. 
+    for k in np.arange(1, n + 1):
+       l: int = (k * (k + 1) / 2)
+       i: int = i.at[(l):(l + k)].set(np.arange(l))
+    # 
 
     return j
 
