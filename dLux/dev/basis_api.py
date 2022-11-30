87513ead6993c354fb8a6bde11a485d979391cfb
Jordan Dennis <jdenn105@gmail.com>
2022-11-30 15:52:11 +1100

    Documenting the `HardCodedLowOrderZernikeBasisAndAperture` class.

--- dLux/dev/basis_api.py
+++ dLux/dev/basis_api.py
@@ -24,25 +24,77 @@ zernikes: list = [
 
 
 class HardCodedLowOrderZernikeBasisAndAperture(eqx.Module):
-    zernikes: list
-    coeffs: list
+    """
+    A selection of the low order (< 10) zernike terms. 
+    This class is the fastest when it comes to applying 
+    the basis terms. 
+
+    Parameters:
+    -----------
+    zernikes: Array
+        An array of `jit` compiled zernike basis functions 
+        that operate on a set of coordinates. In particular 
+        these coordinates correspond to a normalised set 
+        of coordinates that are centered at the the centre 
+        of the circular aperture with 1. occuring along the 
+        radius. 
+    coeffs: Array
+        The coefficients of the Zernike terms. 
+    aperture: Layer
+        Must be an instance of `CircularAperture`. This 
+        is applied alongside the basis. 
+    """
+    zernikes: Array
+    coeffs: Array
     aperture: Layer
 
 
     def __init__(self, noll_inds: list, coeffs: list, aperture: list):
+        """
+        Parameters:
+        -----------
+        noll_inds: Array 
+            The noll indices of the zernikes that are to be mapped 
+            over the aperture.
+        coeffs: Array 
+            The coefficients associated with the zernikes. These 
+            should be ordered by the noll index of the zernike 
+            that they refer to.
+        aperture: Layer
+            A `CircularAperture` within which the aberrations are 
+            being studied. 
+        """
         self.zernikes = [zernikes[ind] for ind in noll_inds]
         self.coeffs = np.asarray(coeffs).astype(float)
+        self.aperture = aperture
 
         assert len(noll_inds) == len(coeffs)
+        assert isinstance(aperture, dl.CircularAperture)
 
 
     def __call__(self, params_dict: dict) -> dict:
+        """
+        Apply the aperture and the abberations to the wavefront.  
+
+        Parameters:
+        -----------
+        params: dict
+            A dictionary containing the key "Wavefront".
+
+        Returns:
+        --------
+        params: dict 
+            A dictionary containing the key "wavefront".
+        """
         wavefront: object = params_dict["Wavefront"]
         cart_coords: list = wavefront.pixel_positions()
         pol_coords: list = dl.cartesian_to_polar(car_coords)
         rho: list = pol_coords[0]
         theta: list = pol_coords[1]
         basis: list = np.stack([z(rho, theta) for z in zernikes])
-        opd: list = np.dot(basis.T, self.coeffs)
-        params_dict["Wavefront"] = wavefront.add_opd(opd)
+        aperture: Array = self.aperture._aperture(cart_coords)
+        opd: list = np.dot(basis.T, self.coeffs) * aperture
+        params_dict["Wavefront"] = wavefront\
+            .add_opd(opd)\
+            .multiply_amplitude(aperture)
         return params_dict
