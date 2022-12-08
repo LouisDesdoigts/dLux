1d56836312927c533411303c19e9dd031f8ee5f2
Jordan Dennis <jdenn105@gmail.com>
2022-12-08 16:00:05 +1000

    I got the wedges vectorised which I guess is a start. I am still stuck though on fixing the inside to be negative. I'm pooped so I will call it a day now.

--- dLux/dev/polygons.py
+++ dLux/dev/polygons.py
@@ -16,12 +16,12 @@ neg_pi_to_pi_phi: float = np.arctan2(coords[1], coords[0])
 phi: float = neg_pi_to_pi_phi + 2. * (neg_pi_to_pi_phi < 0.) * np.pi
 r: float = np.hypot(coords[0], coords[1])
 
-i: int = 2
+i: int = np.arange(n)
 low_bound: float = 2. * i * alpha
 top_bound: float = 2. * (i + 1.) * alpha
 
 x: float = coords[0]
-wedge: float = ((low_bound < phi) & (phi < top_bound)).astype(float)
+wedge: float = ((low_bound[:, None, None] < phi) & (phi < top_bound[:, None, None])).astype(float)
 dist_from_rad: float = rmax * np.cos(alpha)
 
 print(dist_from_rad)
@@ -33,10 +33,20 @@ dist: float = x + dist_from_rad
 
 abs_max = lambda arr: np.abs(arr).max()
 
-plt.title(f"$\\phi_{i}$")
-plt.imshow(dist * wedge, vmin=-abs_max(dist), vmax=abs_max(dist))
-plt.colorbar()
-plt.show()
+for _i in i:
+    plt.title(f"$\\phi_{i}$")
+    plt.imshow(wedge[_i], vmin=-abs_max(dist), vmax=abs_max(dist))
+    plt.colorbar()
+    plt.show()
+
+# OK I am stuck again. Well the positive side is always on the right.
+# At least that is for a vertical line. I am not so sure how to 
+# discuss this for a line that is not nessecarily ... Actually I 
+# think that I just do this with abs and then check that if 
+# r < my_line then multiply by negative one.
+
+# I can also just check that the line is negative on the inside 
+# how? 
 
 # So I need to decipher what this does.
 # phi + alpha rotates the whole thing backwards so that zero is 
