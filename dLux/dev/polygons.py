c28f40826f21ad17c495a39edfd3e228438f038f
Jordan Dennis <jdenn105@gmail.com>
2022-12-08 15:04:06 +1000

    Despite the fact that I have done this a hundred times or more I still take a few moments to get the angular coordinates onto the range zero to two pi.

--- dLux/dev/polygons.py
+++ dLux/dev/polygons.py
@@ -11,10 +11,18 @@ alpha: float = np.pi / n # Half the angular disp of one wedge
 grid: float = np.linspace(0, 1., 100) - .5
 coords: float = np.meshgrid(grid, grid)
 
-phi: float = np.arctan2(coords[1], coords[0])
+neg_pi_to_pi_phi: float = np.arctan2(coords[1], coords[0]) 
+phi: float = neg_pi_to_pi_phi + 2. * (neg_pi_to_pi_phi < 0.) * np.pi
 r: float = np.hypot(coords[0], coords[1])
 
 wedges: float = np.floor((phi + alpha) / (2. * alpha))
+# So I need to decipher what this does.
+# phi + alpha rotates the whole thing backwards so that zero is 
+# now along the spoke connecting the bottom of the right-most 
+# edge. By dividing by the angular width of the edge it is 
+# then able to enumerate the edges. This implies that I 
+# probably want to add pi so that I am working on the 
+# range zero two pi. 
 
 abs_max = lambda arr: np.abs(arr).max()
 
@@ -28,9 +36,9 @@ plt.show()
 #plt.colorbar()
 #plt.show()
 #
-#plt.title("$\\phi$")
-#plt.imshow(phi, vmin=-abs_max(phi), vmax=abs_max(phi))
-#plt.colorbar()
-#plt.show()
+plt.title("$\\phi$")
+plt.imshow(phi, vmin=-abs_max(phi), vmax=abs_max(phi))
+plt.colorbar()
+plt.show()
 
 
