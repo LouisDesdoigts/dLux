bf3fb2c87bccc191b6742086c63f63e7c4fad443
Jordan Dennis <jdenn105@gmail.com>
2022-12-08 15:20:33 +1000

    Making some progress on the algorithm but running into all the same problems as before. I think that the best approach will be to get a single slice going.

--- dLux/dev/polygons.py
+++ dLux/dev/polygons.py
@@ -12,10 +12,22 @@ grid: float = np.linspace(0, 1., 100) - .5
 coords: float = np.meshgrid(grid, grid)
 
 neg_pi_to_pi_phi: float = np.arctan2(coords[1], coords[0]) 
-phi: float = neg_pi_to_pi_phi + 2. * (neg_pi_to_pi_phi < 0.) * np.pi
+off_phi: float = neg_pi_to_pi_phi + 2. * (neg_pi_to_pi_phi < 0.) * np.pi
+phi: float = off_phi - alpha
 r: float = np.hypot(coords[0], coords[1])
 
 wedges: float = np.floor((phi + alpha) / (2. * alpha))
+wedges: float = np.tile(phi, (n, 1, 1))
+
+abs_max = lambda arr: np.abs(arr).max()
+
+for i in range(n): 
+    wedge = (i == np.floor((phi + alpha) / (2. * alpha))) * phi
+    plt.title(f"$\\phi_{i}$")
+    plt.imshow(wedge, vmin=-abs_max(wedges[i]), vmax=abs_max(wedges[i]))
+    plt.colorbar()
+    plt.show()
+
 # So I need to decipher what this does.
 # phi + alpha rotates the whole thing backwards so that zero is 
 # now along the spoke connecting the bottom of the right-most 
@@ -24,12 +36,6 @@ wedges: float = np.floor((phi + alpha) / (2. * alpha))
 # probably want to add pi so that I am working on the 
 # range zero two pi. 
 
-abs_max = lambda arr: np.abs(arr).max()
-
-plt.title("$\\phi_{w}$")
-plt.imshow(wedges, vmin=-abs_max(wedges), vmax=abs_max(wedges))
-plt.colorbar()
-plt.show()
 
 #plt.title("$r$")
 #plt.imshow(r, vmin=-abs_max(r), vmax=abs_max(r))
