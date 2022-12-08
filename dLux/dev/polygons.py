4875c902227f2ab837ae5ffd4a42ca2bf48b1963
Jordan Dennis <jdenn105@gmail.com>
2022-12-08 15:27:17 +1000

    I have managed to get a good looking wedge for one case.

--- dLux/dev/polygons.py
+++ dLux/dev/polygons.py
@@ -12,21 +12,21 @@ grid: float = np.linspace(0, 1., 100) - .5
 coords: float = np.meshgrid(grid, grid)
 
 neg_pi_to_pi_phi: float = np.arctan2(coords[1], coords[0]) 
-off_phi: float = neg_pi_to_pi_phi + 2. * (neg_pi_to_pi_phi < 0.) * np.pi
-phi: float = off_phi - alpha
+phi: float = neg_pi_to_pi_phi + 2. * (neg_pi_to_pi_phi < 0.) * np.pi
 r: float = np.hypot(coords[0], coords[1])
 
-wedges: float = np.floor((phi + alpha) / (2. * alpha))
-wedges: float = np.tile(phi, (n, 1, 1))
+i: int = 2
+low_bound: float = 2. * i * alpha
+top_bound: float = 2. * (i + 1.) * alpha
+
+wedge = ((low_bound < phi) & (phi < top_bound)).astype(float)
 
 abs_max = lambda arr: np.abs(arr).max()
 
-for i in range(n): 
-    wedge = (i == np.floor((phi + alpha) / (2. * alpha))) * phi
-    plt.title(f"$\\phi_{i}$")
-    plt.imshow(wedge, vmin=-abs_max(wedges[i]), vmax=abs_max(wedges[i]))
-    plt.colorbar()
-    plt.show()
+plt.title(f"$\\phi_{i}$")
+plt.imshow(wedge, vmin=-abs_max(wedge), vmax=abs_max(wedge))
+plt.colorbar()
+plt.show()
 
 # So I need to decipher what this does.
 # phi + alpha rotates the whole thing backwards so that zero is 
@@ -41,10 +41,5 @@ for i in range(n):
 #plt.imshow(r, vmin=-abs_max(r), vmax=abs_max(r))
 #plt.colorbar()
 #plt.show()
-#
-plt.title("$\\phi$")
-plt.imshow(phi, vmin=-abs_max(phi), vmax=abs_max(phi))
-plt.colorbar()
-plt.show()
 
 
