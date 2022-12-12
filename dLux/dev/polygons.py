830a3644d2d966465299873d17a69e501cb2134e
Jordan Dennis <jdenn105@gmail.com>
2022-12-12 10:03:35 +1000

    Running into the interesting problem that `jax.lax.cond` is not broadcastable. This implies that either a `jax.for_loop` is required or I need to make `jax.lax.cond` broadcastable with `jax.vmap`. This raises an interesting problem with the `in_axes` specification since `jax.lax.cond` has an arbitrary number of input arguments.

--- dLux/dev/polygons.py
+++ dLux/dev/polygons.py
@@ -80,6 +80,19 @@ y1: float = vertices[:, 1]
 x: float = coords[0][:, :, None]
 y: float = coords[1][:, :, None]
 
+vcond: callable = jax.vmap(jax.lax.cond, in_axes=(0, None, None, 0, 0))
+
+
+def dist_from_line(m: float, x1: float, y1: float, x: float, y: float) -> float:
+    inf_case: callable = lambda: np.abs(x - x1)
+    gen_case: callable = lambda: np.abs(m * (x - x1) - (y - y1)) / np.sqrt(1 + m ** 2)
+    return vcond(np.isinf(m), inf_case, gen_case)
+
+
+d:float = dist_from_line(m, x1, y1, x, y)
+
+vcond(np.isinf(m), lambda: m, lambda: )
+
 d: float = np.abs(m * (x - x1) - (y - y1)) / np.sqrt(1 + m ** 2)
 theta: float = np.arctan2(y1, x1)
 
