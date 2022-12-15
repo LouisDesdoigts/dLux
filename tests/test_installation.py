866cdcd1a81a3dbc9b5a26e9033d6dbed7ec99b6
Jordan Dennis <jdenn105@gmail.com>
2022-12-16 09:52:15 +1000

    Starting to develop tests from installation.

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -2,5 +2,14 @@ import pytest
 import sys
 import os
 
-sys.stdin
+
+not_
+platform: str = sys.platform
+
+if platform not in ["linux", "linux2"]:
+    print(f"Error: {not_lin_err}")
+
+os.system("pip install --quiet .")
+os.system("rm -r dLux.egg-info")
+os.system("rm -r build")
 
