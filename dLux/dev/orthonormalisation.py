9247a1c487a553011e06ef33b4f460db1bc95ca3
Jordan Dennis <jdenn105@gmail.com>
2022-12-15 11:46:49 +1000

    Moving the `_orthonormalise` function into `orthonormalisation.py`.

---
+++ dLux/dev/orthonormalisation.py
@@ -0,0 +1,24 @@
+
+pixel_area = aperture.sum()
+shape = zernikes.shape
+width = shape[-1]
+basis = np.zeros(shape).at[0].set(aperture)
+
+for j in np.arange(1, self.nterms):
+    intermediate = zernikes[j] * aperture
+    coefficient = np.zeros((self.nterms, 1, 1), dtype=float)
+    mask = (np.arange(1, self.nterms) > j + 1).reshape((-1, 1, 1))
+
+    coefficient = -1 / pixel_area * \
+        (zernikes[j] * basis[1:] * aperture * mask)\
+        .sum(axis = (1, 2))\
+        .reshape(-1, 1, 1) 
+
+    print(coefficient)
+
+    intermediate += (coefficient * basis[1:] * mask).sum(axis = 0)
+    
+    basis = basis\
+        .at[j]\
+        .set(intermediate / \
+            np.sqrt((intermediate ** 2).sum() / pixel_area))
