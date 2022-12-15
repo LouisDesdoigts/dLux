8773ecc72ca34d69fb9d9989394ef5da8ba809bb
Jordan Dennis <jdenn105@gmail.com>
2022-12-15 14:17:55 +1000

    Removing the old implementation of the GS orthonormalisation.

--- dLux/dev/orthonormalisation.py
+++ dLux/dev/orthonormalisation.py
@@ -36,44 +36,6 @@ aperture: float = aper._aperture(coords)
 zcoords: float = aper._normalised_coordinates(coords)
 zernikes: float = np.stack([h(zcoords) for h in basis.basis_funcs])
 
-# +
-# for j in np.arange(1, nterms):
-# -
-
-pixel_area: float = aperture.sum()
-shape: tuple = zernikes.shape
-width: int = shape[-1]
-_basis: float = np.zeros(shape).at[0].set(aperture)
-j: int = 1
-
-plot_basis(_basis)
-
-intermediate = zernikes[j] * aperture
-coefficient = np.zeros((nterms, 1, 1), dtype=float)
-mask = (np.arange(1, nterms) > j + 1)[:, None, None].astype(float)
-
-mask.flatten()
-
-# So I have identified the problem. `_basis[1:]` is identically zero. 
-
-coefficient = -1. / pixel_area * (zernikes[j] * _basis[1:] * aperture * mask)
-
-plot_basis(coefficient)
-
-coefficient = -1. / pixel_area * \
-    (zernikes[j] * _basis[1:] * aperture * mask)\
-    .sum(axis = (1, 2))\
-    .reshape(-1, 1, 1) 
-
-intermediate += (coefficient * _basis[1:] * mask).sum(axis = 0)
-
-_basis = _basis\
-    .at[j]\
-    .set(intermediate / \
-        np.sqrt((intermediate ** 2).sum() / pixel_area))
-
-plot_basis(_basis)
-
 
 def inner_product(f1: float, f2: float) -> float:
     return (f1 * f2).sum()
