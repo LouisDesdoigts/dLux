d05941d62e0d16a30db4971de365fd470d04bc4b
Jordan Dennis <jdenn105@gmail.com>
2022-12-16 15:19:01 +1000

    Moving the `create_wavefront` `fixture` into `conftest.py`.

--- tests/test_wavefronts.py
+++ tests/test_wavefronts.py
@@ -7,26 +7,6 @@ config.update("jax_debug_nans", True)
 
 Array = np.ndarray
 
-@pytest.fixture
-def create_wavefront():
-    """
-    Returns:
-    --------
-    create_wavefront: callable
-        A function that has all keyword arguments and can be 
-        used to create a wavefront for testing.
-    """
-    def _create_wavefront(
-            wavelength: Array = np.array(550e-09),
-            pixel_scale: Array = np.array(1.),
-            plane_type: int = dLux.PlaneType.Pupil,
-            amplitude: Array = np.ones((1, 16, 16)),
-            phase: Array = np.zeros((1, 16, 16))) -> Wavefront:
-        return dLux.wavefronts.Wavefront(
-            wavelength, pixel_scale, amplitude, phase, plane_type)
-    return _create_wavefront
-
-
 class TestWavefront(object):
     """
     Test the Wavefront class.
