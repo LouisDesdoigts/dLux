fb5338c381fe2e36cb9d22a2b686f67be187fad8
Jordan Dennis <jdenn105@gmail.com>
2022-12-16 13:54:24 +1000

    Adding the `create_angular_wavefront` `fixture`.

--- tests/test_wavefronts.py
+++ tests/test_wavefronts.py
@@ -50,6 +50,24 @@ def create_cartesian_wavefront():
     return _create_wavefront
 
 
+@pytest.fixture
+def create_angular_wavefront():
+    """
+    Returns:
+    --------
+    create_wavefront: callable
+        A function that has all keyword arguments and can be 
+        used to create a wavefront for testing.
+    """
+    def _create_wavefront(
+            wavelength: Array = np.array(550e-09),
+            pixel_scale: Array = np.array(1.),
+            plane_type: int = dLux.PlaneType.Pupil,
+            amplitude: Array = np.ones((1, 16, 16)),
+            phase: Array = np.zeros((1, 16, 16))) -> Wavefront:
+        return dLux.wavefronts.AngularWavefront(
+            wavelength, pixel_scale, amplitude, phase, plane_type)
+    return _create_wavefront
 
 
 class FarFieldFresnelWavefrontUtility(WavefrontUtility):
