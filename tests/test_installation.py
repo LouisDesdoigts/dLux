da6f0499b2ad28535663c250f799f58386a5eaea
Jordan Dennis <jdenn105@gmail.com>
2022-12-16 10:08:01 +1000

    Adding tests for correct installation.

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -2,14 +2,30 @@ import pytest
 import sys
 import os
 
-
-not_
 platform: str = sys.platform
+not_lin_err: str = """
+    The installation test is designed to run on
+    a linux machine. In particular, this test
+    is designed to be run by github actions.
+    If you are using windows dispare.
+"""
 
 if platform not in ["linux", "linux2"]:
     print(f"Error: {not_lin_err}")
 
-os.system("pip install --quiet .")
-os.system("rm -r dLux.egg-info")
-os.system("rm -r build")
+
+def test_install_dLux():
+    install_ok: int = os.system("pip install --quiet .")
+
+    assert install_ok == 0
+
+    os.system("rm -r dLux.egg-info")
+    os.system("rm -r build")
+
+
+def test_import_dLux():
+    import dLux
+    assert True
+    
+
 
