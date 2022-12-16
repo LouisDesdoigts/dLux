92b35a44c71d2feb640b191d87bb45409e14a86b
Jordan Dennis <jdenn105@gmail.com>
2022-12-16 11:13:20 +1000

    Implementing a context in which to learn how to properly use `pytest`.

---
+++ tests/test_fixtures.py
@@ -0,0 +1,39 @@
+import pytest
+import enum
+
+
+class Gender(enum.Enum):
+    MALE = 0
+    FEMALE = 1
+
+
+class Person(object):
+    name: str
+    height: float
+    age: int
+    gender: int
+
+
+    def __init__(self, name: str, height: float, age: int, gender: int):
+        self.name = name
+        self.height = height
+        self.age = age
+        self.gender = gender
+
+
+    def __repr__(self) -> str:
+        out: str = "I am {}, a {} years old {}, who is {:.2f}m tall." 
+        return out.format(self.name, self.age, self.gender, self.height)
+
+
+@pytest.fixture
+def create_john():
+    return Person("John", 1.845, 33, Gender.MALE)
+
+
+@pytest.fixture
+def create_charlie():
+    return Person("Charlie", 1.543, 564)
+
+
+def test_john
