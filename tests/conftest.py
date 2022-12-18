28953649231a08c97cf0e7af5e9201a33df753ff
Jordan Dennis <jdenn105@gmail.com>
2022-12-19 08:49:46 +1000

    Implementing the `create_add_opd` `fixture`.

--- tests/conftest.py
+++ tests/conftest.py
@@ -103,3 +103,17 @@ def create_add_phase():
     return _create_add_phase    
 
 
+@pytest.fixture
+def create_add_opd():
+    """
+    Returns:
+    --------
+    create_add_opd: callable
+        A function that has all keyword arguments and can be
+        used to create a `AddOPD` layer for testing.
+    """
+    def _create_add_opd(opd: Array = np.ones((16, 16))) -> OpticalLayer:
+        return dLux.optics.AddOPD(opd)
+    return _create_add_opd
+
+
