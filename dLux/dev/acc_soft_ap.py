df7391f2a68cbc93cdf944e2536625c5270ac33a
Jordan Dennis <jdenn105@gmail.com>
2023-01-04 14:00:22 +1000

    Removing examination code for the different versions of the occulting.

--- dLux/dev/acc_soft_ap.py
+++ dLux/dev/acc_soft_ap.py
@@ -19,34 +19,16 @@ def occulting_v0(occulting: bool, x: float) -> float:
     return y
 
 
-jax.make_jaxpr(occulting_v0, static_argnums=0)(occulting, x)
-
-# %%timeit
-occulting_v0(occulting, x)
-
-
 @ft.partial(jax.jit, inline=True)
 def occulting_v1(occulting: bool, x: float) -> float:
     occ: float = occulting.astype(float)
     return (1. - x) * occ + (1. - occ) * x
 
 
-jax.make_jaxpr(occulting_v1)(occulting, x)
-
-# %%timeit
-occulting_v1(occulting, x)
-
-
 @ft.partial(jax.jit, inline=True)
 def occulting_v2(occulting: bool, x: float) -> float:
     occ: float = occulting.astype(float)
     return occ + x - 2. * x * occ 
-
-
-jax.make_jaxpr(occulting_v2)(occulting, x)
-
-# %%timeit 
-socculting_v2(occulting, x)
 
 
 # Specify the width of the soft fringe in pixels then use .at and set the value to .5. Discuss this with louis. 
