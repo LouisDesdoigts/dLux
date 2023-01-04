89901d826be96ad1336f6300584eec09d770d1c3
Jordan Dennis <jdenn105@gmail.com>
2023-01-04 11:28:07 +1000

    Addressing #177, but there is the catch that I need to document and scrub the `pixel_coordinates` property. I am just going to check that this has worked first.

--- dLux/wavefronts.py
+++ dLux/wavefronts.py
@@ -60,6 +60,7 @@ class Wavefront(ExtendedBase, ABC):
     plane_type  : PlaneType
     amplitude   : Array
     phase       : Array
+    coordinates : Array
 
 
     def __init__(self        : Wavefront,
@@ -88,6 +89,7 @@ class Wavefront(ExtendedBase, ABC):
         self.pixel_scale = np.asarray(pixel_scale, dtype=float)
         self.amplitude   = np.asarray(amplitude,   dtype=float)
         self.phase       = np.asarray(phase,       dtype=float)
+        self.coordinates = get_pixel_coordinates(self.npixels, pixel_scale)
         self.plane_type  = plane_type
 
         # Input checks
@@ -214,7 +216,7 @@ class Wavefront(ExtendedBase, ABC):
             The coordinates of the centers of each pixel representing the
             wavefront.
         """
-        return get_pixel_coordinates(self.npixels, self.pixel_scale)
+        return self.coordinates
 
 
     @property
