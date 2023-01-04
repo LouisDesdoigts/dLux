afedaabb6001f41cc1b0856cff7c464fbf455964
Jordan Dennis <jdenn105@gmail.com>
2023-01-04 13:42:52 +1000

    Testing access to `jax` at a lower point on a simple example and finding that it can be used to increase speed.

--- dLux/dev/acc_soft_ap.py
+++ dLux/dev/acc_soft_ap.py
@@ -46,6 +46,45 @@ def occulting_v2(occulting: bool, x: float) -> float:
 jax.make_jaxpr(occulting_v2)(occulting, x)
 
 # %%timeit 
-occulting_v2(occulting, x)
+socculting_v2(occulting, x)
 
 
+# Specify the width of the soft fringe in pixels then use .at and set the value to .5. Discuss this with louis. 
+
+# +
+# So I want to do a soft edged aperture. 
+# I also want to test if using the inline=True option is a performance boon.
+# I also want to test writing my own XLA code and binding it to python. 
+# -
+
+@ft.partial(jax.jit, inline=True, static_argnums=0)
+def coords_v1(n: int, rad: float) -> float:
+    axes: float = np.linspace(-rad, rad, n, endpoint=True)
+    return np.asarray(np.meshgrid(axes, axes))
+
+
+@ft.partial(jax.jit, inline=True, static_argnums=0)
+def coords_v2(n: int, rad: float) -> float:
+    arange: float = jax.lax.iota(float, n)
+    max_: float = np.array(n - 1, dtype=float)
+    axes: float = arange * 2. * rad / max_ - rad
+    return np.asarray(np.meshgrid(axes, axes))
+
+
+arange: float = jax.lax.iota(float, 100)
+max_: float = np.array(100 - 1, dtype=float)
+axes: float = arange * 2. / max_ - 1.
+
+test: float = np.tile(axes, (1, 100, 1))
+
+# %%timeit
+coords_v1(100, 1.)
+
+# %%timeit
+coords_v2(100, 1.)
+
+jax.make_jaxpr(coords_v1, static_argnums=0)(100, 1.)
+
+jax.make_jaxpr(coords_v2, static_argnums=0)(100, 1.)
+
+coords: float = 
