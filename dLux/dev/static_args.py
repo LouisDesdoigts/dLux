9c24294e2801f0ff12aa1d39d2732dd3a446667f
Jordan Dennis <jdenn105@gmail.com>
2023-01-09 14:41:19 +1000

    Fixing stash changes.

--- dLux/dev/static_args.py
+++ dLux/dev/static_args.py
@@ -173,7 +173,7 @@ rotation: float = 0.
 pixel_scale: float = 2. * radius / npix
 
 jit_circ_ap_func: callable = jax.jit(circ_ap_func)
-static_jit_circ_ap_func: callable = jax.jit(circ_ap_func, inline=True, static_argnums=(0, 1, 2, 3, 4))
+static_jit_circ_ap_func: callable = jax.jit(circ_ap_func, inline=True, static_argnums=(1, 2, 3, 4))
 
 # %%timeit
 jit_circ_ap_func(radius, x, y, rotation, nsoft, pixel_scale).block_until_ready()
