81dc2664cf6d56a5c7c06e9723207c3ed490242f
Louis Desdoigts <ldes9555@uni.sydney.edu.au>
2023-02-11 17:47:46 +1100

    Building out the angular conversions in the utils/units.py script

--- dLux/utils/units.py
+++ dLux/utils/units.py
@@ -1,14 +1,23 @@
 import jax.numpy as np
 
-__all__ = ["radians_to_arcseconds", "arcseconds_to_radians"]
-
+__all__ = ["radians_to_arcseconds", "radians_to_degrees", 
+           "radians_to_arcminutes", "degrees_to_radians", 
+           "degrees_to_arcminutes", "degrees_to_arcseconds", 
+           "arcminutes_to_radians", "arcminutes_to_degrees", 
+           "arcminutes_to_arcseconds", "arcseconds_to_radians", 
+           "arcseconds_to_degrees", "arcseconds_to_arcminutes",
+           "r2s", "r2d", "r2m", "d2r", "d2m", "d2s", 
+           "m2r", "m2d", "m2s", "s2r", "s2d", "s2m"]
 
 Array = np.ndarray
 
 
+# Radians to:
 def radians_to_arcseconds(values : Array) -> Array:
     """
     Converts the inputs values from radians to arcseconds.
+
+    Can also be imported as r2s.
 
     Parameters
     ----------
@@ -23,9 +32,165 @@ def radians_to_arcseconds(values : Array) -> Array:
     return values * 3600 * 180 / np.pi
 
 
+def radians_to_degrees(values : Array) -> Array:
+    """
+    Converts the inputs values from radians to degrees.
+
+    Can also be imported as r2d.
+
+    Parameters
+    ----------
+    values : Array, radians
+        The input values in units of radians to be converted into degrees.
+
+    Returns
+    -------
+    values : Array, degrees
+        The input values converted into degrees.
+    """
+    return values * 180 / np.pi
+
+
+def radians_to_arcminutes(values : Array) -> Array:
+    """
+    Converts the inputs values from radians to arcminutes.
+
+    Can also be imported as r2m.
+
+    Parameters
+    ----------
+    values : Array, radians
+        The input values in units of radians to be converted into arcminutes.
+
+    Returns
+    -------
+    values : Array, arcminutes
+        The input values converted into arcminutes.
+    """
+    return values * 60 * 180 / np.pi
+
+
+# Degrees to:
+def degrees_to_radians(values : Array) -> Array:
+    """
+    Converts the inputs values from degrees to radians.
+
+    Can also be imported as d2r.
+
+    Parameters
+    ----------
+    values : Array, degrees
+        The input values in units of degrees to be converted into radians.
+
+    Returns
+    -------
+    values : Array, radians
+        The input values converted into radians.
+    """
+    return values * np.pi / 180
+
+
+def degrees_to_arcminutes(values : Array) -> Array:
+    """
+    Converts the inputs values from degrees to arcminutes.
+
+    Can also be imported as d2m.
+
+    Parameters
+    ----------
+    values : Array, degrees
+        The input values in units of degrees to be converted into arcminutes.
+
+    Returns
+    -------
+    values : Array, arcminutes
+        The input values converted into arcminutes.
+    """
+    return values * 60
+
+
+def degrees_to_arcseconds(values : Array) -> Array:
+    """
+    Converts the inputs values from degrees to arcseconds.
+
+    Can also be imported as d2s.
+
+    Parameters
+    ----------
+    values : Array, degrees
+        The input values in units of degrees to be converted into arcseconds.
+
+    Returns
+    -------
+    values : Array, arcseconds
+        The input values converted into arcseconds.
+    """
+    return values * 3600
+
+# Arcminutes to:
+def arcminutes_to_radians(values : Array) -> Array:
+    """
+    Converts the inputs values from arcminutes to radians.
+
+    Can also be imported as m2r.
+
+    Parameters
+    ----------
+    values : Array, arcminutes
+        The input values in units of arcminutes to be converted into radians.
+
+    Returns
+    -------
+    values : Array, radians
+        The input values converted into radians.
+    """
+    return values * np.pi / (60 * 180)
+
+
+def arcminutes_to_degrees(values : Array) -> Array:
+    """
+    Converts the inputs values from arcminutes to degrees.
+
+    Can also be imported as m2d.
+
+    Parameters
+    ----------
+    values : Array, arcminutes
+        The input values in units of arcminutes to be converted into degrees.
+
+    Returns
+    -------
+    values : Array, degrees
+        The input values converted into degrees.
+    """
+    return values / 60
+
+
+def arcminutes_to_arcseconds(values : Array) -> Array:
+    """
+    Converts the inputs values from arcminutes to arcseconds.
+
+    Can also be imported as m2s.
+
+    Parameters
+    ----------
+    values : Array, arcminutes
+        The input values in units of arcminutes to be converted into arcseconds.
+
+    Returns
+    -------
+    values : Array, arcseconds
+        The input values converted into arcseconds.
+    """
+    return values * 60
+
+
+# Arcseconds to:
 def arcseconds_to_radians(values : Array) -> Array:
     """
     Converts the inputs values from arcseconds to radians.
+
+    Can also be imported as s2r.
 
     Parameters
     ----------
@@ -40,4 +205,53 @@ def arcseconds_to_radians(values : Array) -> Array:
     return values * np.pi / (3600 * 180)
 
 
+def arcseconds_to_degrees(values : Array) -> Array:
+    """
+    Converts the inputs values from arcseconds to degrees.
 
+    Can also be imported as s2d.
+
+    Parameters
+    ----------
+    values : Array, arcseconds
+        The input values in units of arcseconds to be converted into degrees.
+
+    Returns
+    -------
+    values : Array, degrees
+        The input values converted into degrees.
+    """
+    return values / 3600
+
+
+def arcseconds_to_arcminutes(values : Array) -> Array:
+    """
+    Converts the inputs values from arcseconds to arcminutes.
+
+    Can also be imported as s2m.
+
+    Parameters
+    ----------
+    values : Array, arcseconds
+        The input values in units of arcseconds to be converted into arcminutes.
+
+    Returns
+    -------
+    values : Array, arcminutes
+        The input values converted into arcminutes.
+    """
+    return values / 60
+
+# Alias to simpler names
+r2s = radians_to_arcseconds
+r2d = radians_to_degrees
+r2m = radians_to_arcminutes
+d2r = degrees_to_radians
+d2m = degrees_to_arcminutes
+d2s = degrees_to_arcseconds
+m2r = arcminutes_to_radians
+m2d = arcminutes_to_degrees
+m2s = arcminutes_to_arcseconds
+s2r = arcseconds_to_radians
+s2d = arcseconds_to_degrees
+s2m = arcseconds_to_arcminutes
---
+++ docs/utils/units/degrees_to_radians.md
@@ -0,0 +1,1 @@
+::: dLux.utils.units.degrees_to_radians
