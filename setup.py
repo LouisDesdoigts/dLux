4a31767c54a4a184d2d72ccb28ec6c9b5339cc58
Louis Desdoigts <ldes9555@uni.sydney.edu.au>
2023-05-30 12:32:35 +1000

    Tightens requirements and changes github test workflow to use the new requirements

--- setup.py
+++ setup.py
@@ -18,14 +18,13 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 # DEPENDENCIES
-# 1. What are the required dependencies?
 with open('requirements.txt') as f:
     install_requires = f.read().splitlines()
-# 2. What dependencies required to run the unit tests? (i.e. `pytest --remote-data`)
 tests_require = ['pytest']
 docs_require = ['optax', 'matplotlib', 'jupyter', 'jupyterlab', 'tqdm', 
-    'chainconsumer', 'nummpyro']
-
+    'chainconsumer', 'numpyro', 'dLuxToliman', "mkdocs", "mkdocs-jupyter", 
+    "mkdocs-same-dir", "mkdocs-autorefs", "mkdocs-simple-plugin", 
+    "mkdocstrings-python", "jupyter_contrib_nbextensions"]
 
 setuptools.setup(
     python_requires='>=3.7,<4.0',
@@ -43,8 +42,10 @@ setuptools.setup(
     },
 
     install_requires=install_requires,
-    tests_require=tests_require,
-    extras_require={'docs': docs_require},
+    extras_require={
+        'docs': docs_require, 
+        'tests' : tests_require
+        },
 
     classifiers=[
         "Programming Language :: Python :: 3",
