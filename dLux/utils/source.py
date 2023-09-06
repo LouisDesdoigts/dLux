776b167c284b28fbb8de4df5de237f6c473e4e29
Louis Desdoigts <ldes9555@uni.sydney.edu.au>
2023-09-06 13:06:04 +1000

    Adds source utils for parametric binary sources

---
+++ dLux/utils/source.py
@@ -0,0 +1,55 @@
+import jax.numpy as np
+from jax import Array
+
+__all__ = [
+    "fluxes_from_contrast",
+    "positions_from_sep",
+]
+
+
+def fluxes_from_contrast(mean_flux: float, contrast: float) -> Array:
+    """
+    Computes the fluxes of a binary object given the mean flux and contrast.
+
+    Parameters
+    ----------
+    mean_flux : float
+        The mean flux of the binary object.
+    contrast : float
+        The contrast of the binary object.
+
+    Returns
+    -------
+    fluxes : Array
+        The flux (flux1, flux2) of the binary object.
+    """
+    # flux_A = 2 * contrast * mean_flux / (1 + contrast)
+    # flux_B = 2 * mean_flux / (1 + contrast)
+    # return np.array([flux_A, flux_B])
+    return 2 * np.array([contrast * mean_flux, mean_flux]) / (1 + contrast)
+
+
+def positions_from_sep(
+    position: Array, separation: float, position_angle: float
+) -> Array:
+    """
+    Computes the on-sky positions of a binary object given the separation and
+    position angle.
+
+    Parameters
+    ----------
+    position : Array, radians
+        The on-sky position of the primary object.
+    separation : float, radians
+        The separation of the binary object.
+    position_angle : float, radians
+        The position angle of the binary object.
+
+    Returns
+    -------
+    position : Array, radians
+        The ((x, y), (x, y)) on-sky position of this object.
+    """
+    r, phi = separation / 2, position_angle
+    sep_vec = np.array([r * np.sin(phi), r * np.cos(phi)])
+    return np.array([position + sep_vec, position - sep_vec])
