4ef2868e9a947a3bd6f74d1b63b767b3cf185951
Louis Desdoigts <ldes9555@uni.sydney.edu.au>
2026-03-10 18:06:32 +0100

    Fixing bug in zernike calling factorial with new safe-case functionality requiring dimensionless inputs

--- src/dLux/utils/zernikes.py
+++ src/dLux/utils/zernikes.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 import jax.numpy as np
-from jax import lax, Array
+from jax import lax, vmap, Array
 import dLux.utils as dlu
 
 __all__ = [
@@ -128,10 +128,10 @@ def zernike_factors(j: int) -> tuple[Array]:
 
     # Calculate c
     sign = lax.pow(-1.0, k)
-    _fact_1 = dlu.factorial(np.abs(n - k))
-    _fact_2 = dlu.factorial(k)
-    _fact_3 = dlu.factorial(((n + m) // 2) - k)
-    _fact_4 = dlu.factorial(((n - m) // 2) - k)
+    _fact_1 = vmap(dlu.factorial)(np.abs(n - k))
+    _fact_2 = vmap(dlu.factorial)(k)
+    _fact_3 = vmap(dlu.factorial)(((n + m) // 2) - k)
+    _fact_4 = vmap(dlu.factorial)(((n - m) // 2) - k)
     c = sign * _fact_1 / _fact_2 / _fact_3 / _fact_4
     return c, k
 
@@ -238,9 +238,7 @@ def zernike(j: int, coordinates: Array, diameter: float = 2) -> Array:
     return aperture * eval_radial(rho, n, c, k) * eval_azimuthal(theta, n, m)
 
 
-def zernike_fast(
-    n: int, m: int, c: Array, k: Array, coordinates: Array
-) -> Array:
+def zernike_fast(n: int, m: int, c: Array, k: Array, coordinates: Array) -> Array:
     """
     Calculates the Zernike polynomial using the pre-calculated c and k parameters, such
     that this function is jittable.
@@ -270,9 +268,7 @@ def zernike_fast(
     return aperture * eval_radial(rho, n, c, k) * eval_azimuthal(theta, n, m)
 
 
-def zernike_basis(
-    js: list[int], coordinates: Array, diameter: float = 2
-) -> Array:
+def zernike_basis(js: list[int], coordinates: Array, diameter: float = 2) -> Array:
     """
     Calculates the Zernike polynomial basis. Note that this function is not-jittable.
 
@@ -293,9 +289,7 @@ def zernike_basis(
     return np.array([zernike(j, coordinates, diameter) for j in js])
 
 
-def polike(
-    nsides: int, j: int, coordinates: Array, diameter: float = 2
-) -> Array:
+def polike(nsides: int, j: int, coordinates: Array, diameter: float = 2) -> Array:
     """
     Calculates the Zernike polynomial on an n-sided aperture. Note that this function
     is not-jittable as is has dynamic array shapes. To use this function in a jittable
@@ -366,9 +360,7 @@ def polike_fast(
     return 1 / r_alpha * zernike_fast(n, m, c, k, coordinates / r_alpha)
 
 
-def polike_basis(
-    nsides: int, js: list[int], coordinates: Array, diameter: float = 2
-):
+def polike_basis(nsides: int, js: list[int], coordinates: Array, diameter: float = 2):
     """
     Calculates the Zernike polynomial basis on an n-sided aperture. Note that this
     function is not-jittable.
